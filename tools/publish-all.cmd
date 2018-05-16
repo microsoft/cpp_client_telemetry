32c3fb738b7ffb14aa7a3f88d95a7bf45bf2ed31
Max Golovanov <maxgolov@microsoft.com>
2018-05-15 17:03:35 -0700

    Modify publishing script. Include Native Win32+Win10 and .NET 4.x SDK for now. No static nuget and no Win10 Managed.

--- tools/publish-all.cmd
+++ tools/publish-all.cmd
@@ -1,7 +1,14 @@
-set /p PackageVersion=<../version.txt
+cd %~dp0
+set PATH=%CD%;%PATH%
+cd ..
+set ROOT=%CD%
+set /p PackageVersion=<%ROOT%\Solutions\version.txt
 
-call publish-nupkg.cmd native\Microsoft.Applications.Telemetry.Windows.%PackageVersion%.nupkg
-call publish-nupkg.cmd uap\Microsoft.Applications.Telemetry.Windows.UAP.%PackageVersion%.nupkg
-call publish-nupkg.cmd net40\Microsoft.Applications.Telemetry.Desktop.%PackageVersion%.nupkg
-call publish-nupkg.cmd static-vc140-MD\Microsoft.Applications.Telemetry.Windows.static-vc140-MD.%PackageVersion%.nupkg
-call publish-nupkg.cmd static-vc140-MT\Microsoft.Applications.Telemetry.Windows.static-vc140-MT.%PackageVersion%.nupkg
+cd %ROOT%\tools
+
+call publish-nupkg.cmd NuGet\native\Microsoft.Applications.Telemetry.Windows.%PackageVersion%.nupkg
+call publish-nupkg.cmd NuGet\net40\Microsoft.Applications.Telemetry.Desktop.%PackageVersion%.nupkg
+
+REM call publish-nupkg.cmd NuGet\uap\Microsoft.Applications.Telemetry.Windows.UAP.%PackageVersion%.nupkg
+REM call publish-nupkg.cmd NuGet\static-vc140-MD\Microsoft.Applications.Telemetry.Windows.static-vc140-MD.%PackageVersion%.nupkg
+REM call publish-nupkg.cmd NuGet\static-vc140-MT\Microsoft.Applications.Telemetry.Windows.static-vc140-MT.%PackageVersion%.nupkg
