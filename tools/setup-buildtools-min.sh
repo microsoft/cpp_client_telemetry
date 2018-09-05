8e74d28e3c4af3bd13cc4fe83b702ac22f494159
Max Golovanov <maxgolov@microsoft.com>
2018-09-05 13:37:29 -0700

    Add memory storage to Linux build and fix permissions

--- tools/setup-buildtools-min.sh
+++ tools/setup-buildtools-min.sh
@@ -13,11 +13,3 @@ apt install -y libsqlite3-dev
 # Stock sqlite may be too old
 #apt install libsqlite3-dev
 apt install -y wget
-
-# Install sqlite 3.22
-export SQLITE_PKG=sqlite-autoconf-3220000
-wget https://www.sqlite.org/2018/$SQLITE_PKG.tar.gz -O /tmp/sqlite-snapshot.tar.gz
-tar -xvf /tmp/sqlite-snapshot.tar.gz
-cd $SQLITE_PKG
-./configure && make && make install
-cd ..
