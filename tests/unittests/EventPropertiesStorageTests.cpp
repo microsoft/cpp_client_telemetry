90ef452db39823b96cc566285e3f311b25c7ec96
Max Golovanov <maxgolov@microsoft.com>
2018-12-05 16:10:42 -0800

    Build break: unit tests broke after field renaming that was done to fix the gcc issue

--- tests/unittests/EventPropertiesStorageTests.cpp
+++ tests/unittests/EventPropertiesStorageTests.cpp
@@ -6,69 +6,69 @@ using namespace ARIASDK_NS;
 
 TEST(EventPropertiesStorageTests, DefaultConstructor)
 {
-	EventPropertiesStorage storage;
-	EXPECT_THAT(storage.EventName, Eq(""));
-	EXPECT_THAT(storage.EventType, Eq(""));
-	EXPECT_THAT(storage.EventLatency, EventLatency_Normal);
-	EXPECT_THAT(storage.EventPersistence, EventPersistence_Normal);
-	EXPECT_THAT(storage.EventPopSample, 100);
-	EXPECT_THAT(storage.EventPolicyBitflags, 0);
-	EXPECT_THAT(storage.TimestampInMillis, 0);
+    EventPropertiesStorage storage;
+    EXPECT_THAT(storage.eventName, Eq(""));
+    EXPECT_THAT(storage.eventType, Eq(""));
+    EXPECT_THAT(storage.eventLatency, EventLatency_Normal);
+    EXPECT_THAT(storage.eventPersistence, EventPersistence_Normal);
+    EXPECT_THAT(storage.eventPopSample, 100);
+    EXPECT_THAT(storage.eventPolicyBitflags, 0);
+    EXPECT_THAT(storage.timestampInMillis, 0);
 }
 
 static EventPropertiesStorage ConstructNonDefaultStorage() noexcept
 {
-	EventPropertiesStorage storage;
-	storage.EventName = "Fred";
-	storage.EventType = "Rogers";
-	storage.EventLatency = EventLatency::EventLatency_CostDeferred;
-	storage.EventPersistence = EventPersistence::EventPersistence_Critical;
-	storage.EventPopSample = 3.14;
-	storage.EventPolicyBitflags = 42;
-	storage.TimestampInMillis = 123;
-	return storage;
+    EventPropertiesStorage storage;
+    storage.eventName = "Fred";
+    storage.eventType = "Rogers";
+    storage.eventLatency = EventLatency::EventLatency_CostDeferred;
+    storage.eventPersistence = EventPersistence::EventPersistence_Critical;
+    storage.eventPopSample = 3.14;
+    storage.eventPolicyBitflags = 42;
+    storage.timestampInMillis = 123;
+    return storage;
 }
 
 TEST(EventPropertiesStorageTests, CopyConstructor)
 {
-	EventPropertiesStorage storage = ConstructNonDefaultStorage();
-	EventPropertiesStorage storageCopy { storage };
+    EventPropertiesStorage storage = ConstructNonDefaultStorage();
+    EventPropertiesStorage storageCopy{ storage };
 
-	EXPECT_THAT(storageCopy.EventName, storage.EventName);
-	EXPECT_THAT(storageCopy.EventType, storage.EventType);
-	EXPECT_THAT(storageCopy.EventLatency, storage.EventLatency);
-	EXPECT_THAT(storageCopy.EventPersistence, storage.EventPersistence);
-	EXPECT_THAT(storageCopy.EventPopSample, storage.EventPopSample);
-	EXPECT_THAT(storageCopy.EventPolicyBitflags, storage.EventPolicyBitflags);
-	EXPECT_THAT(storageCopy.TimestampInMillis, storage.TimestampInMillis);
+    EXPECT_THAT(storageCopy.eventName, storage.eventName);
+    EXPECT_THAT(storageCopy.eventType, storage.eventType);
+    EXPECT_THAT(storageCopy.eventLatency, storage.eventLatency);
+    EXPECT_THAT(storageCopy.eventPersistence, storage.eventPersistence);
+    EXPECT_THAT(storageCopy.eventPopSample, storage.eventPopSample);
+    EXPECT_THAT(storageCopy.eventPolicyBitflags, storage.eventPolicyBitflags);
+    EXPECT_THAT(storageCopy.timestampInMillis, storage.timestampInMillis);
 }
 
 TEST(EventPropertiesStorageTests, MoveConstructor)
 {
-	EventPropertiesStorage storage = ConstructNonDefaultStorage();
-	EventPropertiesStorage storageCopy { std::move(storage) };
+    EventPropertiesStorage storage = ConstructNonDefaultStorage();
+    EventPropertiesStorage storageCopy{ std::move(storage) };
 
-	EXPECT_THAT(storageCopy.EventName, "Fred");
-	EXPECT_THAT(storageCopy.EventType, "Rogers");
-	EXPECT_THAT(storageCopy.EventLatency, EventLatency::EventLatency_CostDeferred);
-	EXPECT_THAT(storageCopy.EventPersistence, EventPersistence::EventPersistence_Critical);
-	EXPECT_THAT(storageCopy.EventPopSample, 3.14);
-	EXPECT_THAT(storageCopy.EventPolicyBitflags, 42);
-	EXPECT_THAT(storageCopy.TimestampInMillis, 123);
+    EXPECT_THAT(storageCopy.eventName, "Fred");
+    EXPECT_THAT(storageCopy.eventType, "Rogers");
+    EXPECT_THAT(storageCopy.eventLatency, EventLatency::EventLatency_CostDeferred);
+    EXPECT_THAT(storageCopy.eventPersistence, EventPersistence::EventPersistence_Critical);
+    EXPECT_THAT(storageCopy.eventPopSample, 3.14);
+    EXPECT_THAT(storageCopy.eventPolicyBitflags, 42);
+    EXPECT_THAT(storageCopy.timestampInMillis, 123);
 }
 
 TEST(EventPropertiesStorageTests, AssignmentOperator)
 {
-	EventPropertiesStorage storage = ConstructNonDefaultStorage();
-	EventPropertiesStorage secondStorage;
-	secondStorage.EventName = "Mister";
-	secondStorage = storage;
+    EventPropertiesStorage storage = ConstructNonDefaultStorage();
+    EventPropertiesStorage secondStorage;
+    secondStorage.eventName = "Mister";
+    secondStorage = storage;
 
-	EXPECT_THAT(secondStorage.EventName, storage.EventName);
-	EXPECT_THAT(secondStorage.EventType, storage.EventType);
-	EXPECT_THAT(secondStorage.EventLatency, storage.EventLatency);
-	EXPECT_THAT(secondStorage.EventPersistence, storage.EventPersistence);
-	EXPECT_THAT(secondStorage.EventPopSample, storage.EventPopSample);
-	EXPECT_THAT(secondStorage.EventPolicyBitflags, storage.EventPolicyBitflags);
-	EXPECT_THAT(secondStorage.TimestampInMillis, storage.TimestampInMillis);
+    EXPECT_THAT(secondStorage.eventName, storage.eventName);
+    EXPECT_THAT(secondStorage.eventType, storage.eventType);
+    EXPECT_THAT(secondStorage.eventLatency, storage.eventLatency);
+    EXPECT_THAT(secondStorage.eventPersistence, storage.eventPersistence);
+    EXPECT_THAT(secondStorage.eventPopSample, storage.eventPopSample);
+    EXPECT_THAT(secondStorage.eventPolicyBitflags, storage.eventPolicyBitflags);
+    EXPECT_THAT(secondStorage.timestampInMillis, storage.timestampInMillis);
 }
