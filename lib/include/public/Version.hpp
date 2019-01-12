bf8672e7626e19c3c94f6d91e73e2d328b3377bd
Max Golovanov <maxgolov@microsoft.com>
2019-01-11 22:11:14 -0800

    2019: update version from 3.0 to 3.1

--- lib/include/public/Version.hpp
+++ lib/include/public/Version.hpp
@@ -2,8 +2,8 @@
 // WARNING: DO NOT MODIFY THIS FILE!
 // Copyright (c) Microsoft. All rights reserved.
 // This file has been automatically generated, manual changes will be lost.
-#define BUILD_VERSION_STR "3.0.339.1"
-#define BUILD_VERSION 3,0,339,1
+#define BUILD_VERSION_STR "3.1.11.1"
+#define BUILD_VERSION 3,1,11,1
 
 #ifndef RESOURCE_COMPILER_INVOKED
 #include <stdint.h>
@@ -23,8 +23,8 @@ namespace ARIASDK_NS_BEGIN {
 
 uint64_t const Version =
 	((uint64_t)3 << 48) |
-	((uint64_t)0 << 32) |
-	((uint64_t)339 << 16) |
+	((uint64_t)1 << 32) |
+	((uint64_t)11 << 16) |
 	((uint64_t)1);
 
 // TODO: [MG] - move declaration of ARIA_SDK_UNUSED to separate include file
