b7f60a1bce67e36e2cf5c19bd1ea6709d6e3ca52
Matt Koscumb <makoscum@microsoft.com>
2019-04-09 10:42:43 -0700

    Default assign ClockSkewManager's members. Add tests.

--- lib/offline/ClockSkewManager.hpp
+++ lib/offline/ClockSkewManager.hpp
@@ -11,17 +11,11 @@ namespace ARIASDK_NS_BEGIN {
     class ClockSkewManager
     {
     public:
-        ClockSkewManager()
-        : m_delta("")
-        , m_pingSent(false)
-        , m_deltaReceived(false)
-        , m_resumeTransmissionAfterClockSkew(false)
-        {
-        }
+        ClockSkewManager() = default;
 
         void handleResponse(const HttpHeaders& headers)
         {
-            std::string timeString = headers.get("time-delta-millis");
+            const auto& timeString = headers.get("time-delta-millis");
             if (!timeString.empty())
             {
                 SetDelta(timeString);
@@ -41,43 +35,39 @@ namespace ARIASDK_NS_BEGIN {
 
         bool isClockSkewOn() const
         {
+            if (!m_pingSent || (m_deltaReceived && !m_delta.empty()))
             {
-                if (!m_pingSent || (m_deltaReceived && !m_delta.empty()))
-                {
-                    return true;
-                }
-                return false;
+               return true;
             }
+            return false;
         }
 
         bool isWaitingForClockSkew()
         {
+            if (!m_deltaReceived && m_pingSent)
             {
-                if (!m_deltaReceived && m_pingSent)
-                {
-                    int64_t timeSincePing = PAL::getUtcSystemTime() - m_pingSendTime;
-                    if (timeSincePing > 30) //30 secs have passed sinse ping was sent, soemthing went wrong, disable clock skew
-                    {
-                        SetDelta("");
-                        return false;
-                    }
-                    return true;
-                }
-                return false;
+               int64_t timeSincePing = PAL::getUtcSystemTime() - m_pingSendTime;
+               if (timeSincePing > 30) //30 secs have passed sinse ping was sent, soemthing went wrong, disable clock skew
+               {
+                  SetDelta("");
+                  return false;
+               }
+               return true;
             }
+            return false;
         }
 
         std::string GetDelta()
         {
+            if (m_pingSent == false)
             {
-                if (m_pingSent == false)
-                {
-                    m_pingSent = true;
-                    m_pingSendTime = PAL::getUtcSystemTime();
-                    return "use-collector-delta";
-                }
-                else
-                    return m_delta;
+               m_pingSent = true;
+               m_pingSendTime = PAL::getUtcSystemTime();
+               return "use-collector-delta";
+            }
+            else
+            {
+               return m_delta;
             }
         }
 
@@ -92,11 +82,11 @@ namespace ARIASDK_NS_BEGIN {
         }
 
     private:
-        std::string             m_delta;
-        bool                    m_pingSent;
-        bool                    m_deltaReceived;
-        bool                    m_resumeTransmissionAfterClockSkew;
-        int64_t                 m_pingSendTime;
+        std::string             m_delta {};
+        bool                    m_pingSent {};
+        bool                    m_deltaReceived {};
+        bool                    m_resumeTransmissionAfterClockSkew {};
+        int64_t                 m_pingSendTime {};
     };
 
 } ARIASDK_NS_END
