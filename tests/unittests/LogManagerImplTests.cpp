9eba5dcfe411691a3d9fcfdcfccfc1ee037944b0
Matt Koscumb <makoscum@microsoft.com>
2019-04-29 10:19:24 -0700

    Add the ability to defer ITelemetrySystem start, making the LogManagerImpl even more testable.

--- tests/unittests/LogManagerImplTests.cpp
+++ tests/unittests/LogManagerImplTests.cpp
@@ -8,22 +8,48 @@ using namespace MAT;
 class TestLogManagerImpl : public LogManagerImpl
 {
 public:
-	TestLogManagerImpl(ILogConfiguration& configuration, IHttpClient* httpClient)
-		: LogManagerImpl(configuration, httpClient) { }
-	using LogManagerImpl::m_httpClient;
-	using LogManagerImpl::m_ownHttpClient;
+   TestLogManagerImpl(ILogConfiguration& configuration, IHttpClient* httpClient)
+      : TestLogManagerImpl(configuration, httpClient, false) { }
+   TestLogManagerImpl(ILogConfiguration& configuration, IHttpClient* httpClient, bool deferSystemStart)
+      : LogManagerImpl(configuration, httpClient, deferSystemStart) { }
+
+   using LogManagerImpl::m_httpClient;
+   using LogManagerImpl::m_ownHttpClient;
+};
+
+class TestHttpClient : public IHttpClient
+{
+   virtual IHttpRequest* CreateRequest() override { return nullptr; }
+   virtual void SendRequestAsync(IHttpRequest*, IHttpResponseCallback*) override { }
+   virtual void CancelRequestAsync(std::string const&) override { }
 };
 
 TEST(LogManagerImplTests, Constructor_HttpClientIsNullptr_ConstructsOwnHttpClient)
 {
-	ILogConfiguration configuration;
-	TestLogManagerImpl logManager { configuration, nullptr };
-	ASSERT_NE(logManager.m_ownHttpClient, nullptr);
+   ILogConfiguration configuration;
+   TestLogManagerImpl logManager { configuration, nullptr };
+   ASSERT_NE(logManager.m_ownHttpClient, nullptr);
 }
 
 TEST(LogManagerImplTests, Constructor_HttpClientIsNullptr_HttpClientAndOwnHttpClientAreSame)
 {
-	ILogConfiguration configuration;
-	TestLogManagerImpl logManager { configuration, nullptr };
-	ASSERT_EQ(logManager.m_ownHttpClient.get(), logManager.m_httpClient);
+   ILogConfiguration configuration;
+   TestLogManagerImpl logManager { configuration, nullptr };
+   ASSERT_EQ(logManager.m_ownHttpClient.get(), logManager.m_httpClient);
+}
+
+TEST(LogManagerImplTests, Constructor_HttpClientIsNotNullptr_DoesNotConstructOwnHttpClient)
+{
+   ILogConfiguration configuration;
+   TestHttpClient httpClient;
+   TestLogManagerImpl logManager { configuration, &httpClient, true };
+   ASSERT_EQ(logManager.m_ownHttpClient, nullptr);
+}
+
+TEST(LogManagerImplTests, Constructor_HttpClientIsNotNullptr_HttpClientIsSet)
+{
+   ILogConfiguration configuration;
+   TestHttpClient httpClient;
+   TestLogManagerImpl logManager { configuration, &httpClient, true };
+   ASSERT_EQ(logManager.m_httpClient, &httpClient);
 }
