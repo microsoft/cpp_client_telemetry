c7bd42e5321cdab0a6a874e5c9b370cab456b5c0
Max Golovanov <maxgolov@microsoft.com>
2019-05-07 22:45:42 +0000

    Merged PR 1035335: Obj-C to C API sample app
    Obj-C to C API sample app: trivial example that shows how to invoke C API from Cocoa Objective-C application.
    
    Key objectives:
    * demonstrate what set of libraries have to be linked in Cocoa app
    * command-line build using CMake that generates XCode project

---
+++ examples/objc/cocoa-app/AppDelegate.h
@@ -0,0 +1,7 @@
+#import <AppKit/NSApplication.h> // NSApplicationDelegate
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign, nonatomic) IBOutlet NSWindow *window;
+
+@end
