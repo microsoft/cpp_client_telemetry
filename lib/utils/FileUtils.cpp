6f1ace3bcbf1b4fbff658b0c5cc332064bb70543
Max Golovanov <maxgolov@microsoft.com>
2019-05-11 02:44:16 +0000

    Merged PR 1038601: MS Edge (Chromium) started building with libc++ instead of msvcrt on Win32.
    MS Edge (Chromium) started building with libc++ instead of msvcrt on Win32.
    Unfortunately that means that std::ifstream(std::wstring) constructor is not avail..
    Gotta be creative to use Win32 API File I/O instead of C++11 MSFT extensions to
    properly handle text file I/O for UTF-8 filenames, for the user analytics cookie
    feature to work properly until we upgrade to C++17, which would have the standard
    cross-plat portable way of managing UTF-8 filenames.
    
    Related work items: #1781095

--- lib/utils/FileUtils.cpp
+++ lib/utils/FileUtils.cpp
@@ -9,22 +9,41 @@
 namespace ARIASDK_NS_BEGIN
 {
 
-    /// <summary>
-    /// Return file size by filename
-    /// </summary>
-    /// <param name="filename">Filename</param>
-    /// <returns>File size</returns>
+    /**
+     * Return file size by filename.
+     *
+     * @param       filename    UTF-8 filename
+     * @return      File size
+     */
     size_t FileGetSize(const char* filename)
     {
 #ifdef _WIN32
+#ifndef _WINRT
+        /* Use Win32 Desktop API to get file name. Cannot rely on ifstream(wstring) if built with libc++ instead of msvcrt++ */
+        LARGE_INTEGER largeInt;
+        std::wstring filename_w = to_utf16_string(filename);
+        HANDLE hFile = CreateFileW(filename_w.c_str(), GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+        GetFileSizeEx(hFile, &largeInt);
+        CloseHandle(hFile);
+        return (size_t)(largeInt.QuadPart);
+#else
+        /* Building for WinRT / UWP requires msvcrt++ runtime - CreateFileW API is 'Desktop only' */
         std::wstring filename_w = to_utf16_string(filename);
         std::ifstream in(filename_w.c_str(), std::ifstream::ate | std::ifstream::binary);
+        return (size_t)(in.tellg());
+#endif
 #else
         std::ifstream in(filename, std::ifstream::ate | std::ifstream::binary);
-#endif
         return (size_t)(in.tellg());
+#endif
     }
 
+    /**
+     * Delete file.
+     *
+     * @param       filename    UTF-8 flename
+     * @return      0 upon success or non-zero value on error.
+     */
     int FileDelete(const char* filename)
     {
         int result = 0;
@@ -39,6 +58,14 @@ namespace ARIASDK_NS_BEGIN
         return result;
     }
 
+    /**
+     * Open file stream.
+     *
+     * @param       filename    UTF-8 filename
+     * @param       mode        File mode:
+     *
+     * @return      File stream on success, null pointer on failure.
+     */
     std::FILE* FileOpen(const char* filename, const char *mode)
     {
 #ifdef _WIN32
@@ -52,6 +79,12 @@ namespace ARIASDK_NS_BEGIN
 #endif
     }
 
+    /**
+     * Close file stream.
+     *
+     * @param       _Stream    File stream
+     * @return      0 on success, EOF otherwise
+     */
     int FileClose(std::FILE* _Stream)
     {
         return std::fclose(_Stream);
@@ -60,42 +93,77 @@ namespace ARIASDK_NS_BEGIN
     /**
      * Read file contents into std::string.
      *
-     * @param       flename
-     * @return      File contents
+     * @param       UTF-8 flename
+     * @return      File contents. Supports only UTF-8 or ASCII-encoded text files.
      */
     std::string FileGetContents(const char *filename)
     {
 #ifdef _WIN32
-        std::ifstream t(to_utf16_string(filename));
+        char buff[256] = { 0 };
+        std::string result;
+        FILE* fp = FileOpen(filename, "r");
+        if (fp != nullptr)
+        {
+            while (fgets(buff, sizeof(buff), fp))
+            {
+                result += buff;
+            }
+            FileClose(fp);
+        }
+        return result;
 #else
         std::ifstream t(filename);
-#endif
         std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
         return str;
+#endif
     }
 
+    /**
+     * Writes the C string pointed by contents to the file named filename (UTF-8).
+     *
+     * @param       flename    UTF-8 filename
+     * @param       contents   File contents in UTF-8 or ASCII
+     *
+     * @return      true on success, false on failure
+     */
     bool FileWrite(const char* filename, const char* contents)
     {
 #ifdef _WIN32
-        std::ofstream t(to_utf16_string(filename), std::ofstream::out | std::ofstream::trunc);
+        bool result = false;
+        FILE* fp = FileOpen(filename, "w+");
+        if (fp != nullptr)
+        {
+            result = (fputs(contents, fp) >= 0);
+            FileClose(fp);
+        }
+        return result;
 #else
         std::ofstream t(filename, std::ofstream::out | std::ofstream::trunc);
-#endif
         if (t.is_open())
         {
             t << contents;
             return true;
         }
         return false;
+#endif
     }
 
-    bool FileExists(const char* name) {
+    /**
+     * Check if file exists.
+     *
+     * @param       name    UTF-8 file name
+     * @return      File contents
+     */
+    bool FileExists(const char* name)
+    {
 #ifdef _WIN32
-        std::ifstream t(to_utf16_string(name));
+        std::wstring filename_w = to_utf16_string(name);
+        DWORD dwAttrib = GetFileAttributesW(filename_w.c_str());
+        return (dwAttrib != INVALID_FILE_ATTRIBUTES && !(dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
 #else
         std::ifstream t(name);
-#endif
         return t.good();
+#endif
     }
 
 } ARIASDK_NS_END
