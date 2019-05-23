880974588c69ff06fd0219892c3652426add1211
Max Golovanov <maxgolov@microsoft.com>
2019-05-23 16:00:03 -0700

    Refresh sample app for C# and add comment how to use UTC (no functional changes to SDK)

--- examples/cs/SampleCsUWP/MainPage.xaml.cs
+++ examples/cs/SampleCsUWP/MainPage.xaml.cs
@@ -1,13 +1,10 @@
-﻿using Microsoft.Applications.Telemetry.Windows;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -16,6 +13,9 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+using Microsoft.Applications.Telemetry.Windows;
+using Windows.UI.Popups;
+using System.Threading.Tasks;
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace SampleCsUWP
