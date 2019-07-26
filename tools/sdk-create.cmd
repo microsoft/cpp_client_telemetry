d34b722fb51af0eb2d7deeabdae0ff580d88b88d
Trevor Lacey <tlacey@microsoft.com>
2019-07-25 17:16:39 -0700

    Parse json objects, fix SDK creation

--- tools/sdk-create.cmd
+++ tools/sdk-create.cmd
@@ -46,4 +46,4 @@ call sku-create.cmd win32-lib-vs2015-md  win32-lib
 call sku-create.cmd win32-lib-vs2015-md  sqlite
 call sku-create.cmd win32-lib-vs2015-md  zlib
 
-copy /Y %ROOT%\docs\release-notes.md %OUTDIR%\
+copy /Y %ROOT%\CHANGELOG.md %OUTDIR%\
