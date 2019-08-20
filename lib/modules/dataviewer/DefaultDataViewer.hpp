f56fa6e90632808420cfbf5b8acd86a938c4cdf8
Sid Dahiya <sidahiy@microsoft.com>
2019-08-20 10:06:04 -0700

    Add default data viewer impl, references and build logic

---
+++ lib/modules/dataviewer/DefaultDataViewer.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include "mat/config.h"
+
+#ifndef DEFAULTDATAVIEWER_HPP
+#define DEFAULTDATAVIEWER_HPP
+
+#include "public/Version.hpp"
+#include "public/IDataViewer.hpp"
+#include "public/ctmacros.hpp"
+
+namespace ARIASDK_NS_BEGIN {
+
+    class MATSDK_LIBABI DefaultDataViewer final : public IDataViewer
+    {
+    public:
+        void RecieveData(const std::vector<std::uint8_t>& packetData) const noexcept override;
+
+        const char* const GetName() const noexcept override
+        {
+            return m_name;
+        }
+
+        bool EnableRemoteViewer(const char * endpoint);
+        bool EnableLocalViewer();
+        bool EnableLocalViewer(const std::string& AppId, const std::string& AppPackage);
+        bool DisableViewer();
+
+    private:
+       static constexpr const char* m_name = "DefaultDataViewer";
+    };
+
+} ARIASDK_NS_END
+
+#endif
