29ce195f3593b1e7c7faebe0fa74b73ff2d8aab1
Sid Dahiya <sidahiy@microsoft.com>
2019-08-20 10:06:04 -0700

    Add default data viewer impl, references and build logic

---
+++ lib/modules/dataviewer/DefaultDataViewer.cpp
@@ -0,0 +1,10 @@
+#include "DefaultDataViewer.hpp"
+
+namespace ARIASDK_NS_BEGIN {
+
+void DefaultDataViewer::RecieveData(const std::vector<std::uint8_t>& /*packetData*/) const noexcept
+{
+
+}
+
+} ARIASDK_NS_END
