09db3047d7375e126fb0c62cbe2bbea69a80b322
Sid Dahiya <sidahiy@microsoft.com>
2019-08-21 00:09:28 -0700

    Add more implementation for DefaultDataViewer

--- lib/modules/dataviewer/DefaultDataViewer.hpp
+++ lib/modules/dataviewer/DefaultDataViewer.hpp
@@ -6,13 +6,19 @@
 
 #include "public/Version.hpp"
 #include "public/IDataViewer.hpp"
+#include "public/IHttpClient.hpp"
 #include "public/ctmacros.hpp"
+
+#include <atomic>
+#include <queue>
 
 namespace ARIASDK_NS_BEGIN {
 
-    class MATSDK_LIBABI DefaultDataViewer final : public IDataViewer
+    class DefaultDataViewer final : public IDataViewer
     {
     public:
+        DefaultDataViewer(std::shared_ptr<IHttpClient> httpClient);
+
         void RecieveData(const std::vector<std::uint8_t>& packetData) const noexcept override;
 
         const char* const GetName() const noexcept override
@@ -20,13 +26,24 @@ namespace ARIASDK_NS_BEGIN {
             return m_name;
         }
 
-        bool EnableRemoteViewer(const char * endpoint);
+        bool EnableRemoteViewer(const char* endpoint);
+
         bool EnableLocalViewer();
+
         bool EnableLocalViewer(const std::string& AppId, const std::string& AppPackage);
-        bool DisableViewer();
+        bool DisableViewer() noexcept;
 
     private:
-       static constexpr const char* m_name = "DefaultDataViewer";
+        void ProcessQueue();
+
+        static constexpr const char* m_name = "DefaultDataViewer";
+        std::shared_ptr<MAT::IHttpClient> m_httpClient;
+
+        std::atomic<bool> m_isTransmissionEnabled;
+        const char* m_endpoint;
+
+        mutable std::queue<std::vector<std::uint8_t>> m_packetQueue;
+
     };
 
 } ARIASDK_NS_END
