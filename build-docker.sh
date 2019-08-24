b599e13c87a124ce6dd0a4dae56e85e49d0e7635
Trevor Lacey <36862671+trevorlacey-msft@users.noreply.github.com>
2019-08-24 10:04:26 -0700

    Update docker scripts for Linux (#64)
    * Add debian 9 docker image
    
    * Run docker in non-tty mode
    
    * Non-TTY docker on Windows
    
    * Change current working directory
    
    * Working path, gitignore
    
    * Fix docker cwd
    
    * Fix cwd again
    
    * Fix build
    
    * Test user
    
    * Update all images to run as root
    
    * Update JSON
    
    * Add build type param
    
    * Updgrade CentOS to support <codecvt>
    
    * Revert devtoolset-8 change
    
    * revert json update
    
    * Use devtoolset-7 image for centos
    
    * Update centos docker image as root
    
    * Install static zlib on centos
    
    * Try again
    
    * Fix vsdevcmd.bat import

--- build-docker.sh
+++ build-docker.sh
@@ -5,10 +5,11 @@ docker info
 docker version
 
 export IMAGE_NAME=$1
+export BUILD_TYPE=$2
 echo Running in container $IMAGE_NAME
 
 echo Building docker image...
 docker build --rm -t $IMAGE_NAME docker/$IMAGE_NAME
 
 echo Starting build...
-docker run -it -v `pwd`:/build $IMAGE_NAME /build/build.sh
+docker run -v `pwd`:/build -w /build $IMAGE_NAME ./build.sh $BUILD_TYPE
