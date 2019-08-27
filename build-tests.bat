a92349bb907494b46c6c5d7ef209b91960d0dd7d
Max Golovanov <maxgolov@microsoft.com>
2019-08-27 15:39:27 -0700

    Fix path to vs2017 for GitHub actions to work

--- build-tests.bat
+++ build-tests.bat
@@ -2,20 +2,26 @@
 call tools\gen-version.cmd
 @setlocal ENABLEEXTENSIONS
 
+set PLATFORM=
+
 REM Possible platforms: Win32|x64
-set PLATFORM=%1
+set PLAT=%1
 REM Possible configurations: Release|Debug
 set CONFIGURATION=%2
 
-echo "Building using Visual Studio 2017 tools"
+REM Add path to vs2017 MSBuild.exe
+set "PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\"
+
+REM Try to setup vs2017 Dev environment if possible
+echo Building using Visual Studio 2017 tools
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
 
 set MAXCPUCOUNT=%NUMBER_OF_PROCESSORS%
 set SOLUTION=Solutions\MSTelemetrySDK.sln
 
-msbuild %SOLUTION% /target:sqlite,zlib,Tests\gmock,Tests\gtest,Tests\UnitTests,Tests\FuncTests /p:BuildProjectReferences=true /maxcpucount:%MAXCPUCOUNT% /detailedsummary /p:Configuration=%CONFIGURATION% /p:Platform=%PLATFORM%
+msbuild %SOLUTION% /target:sqlite,zlib,Tests\gmock,Tests\gtest,Tests\UnitTests,Tests\FuncTests /p:BuildProjectReferences=true /maxcpucount:%MAXCPUCOUNT% /detailedsummary /p:Configuration=%CONFIGURATION% /p:Platform=%PLAT%
 if errorLevel 1 goto end
-Solutions\out\%CONFIGURATION%\%PLATFORM%\UnitTests\UnitTests.exe
+Solutions\out\%CONFIGURATION%\%PLAT%\UnitTests\UnitTests.exe
 if errorLevel 1 goto end
-Solutions\out\%CONFIGURATION%\%PLATFORM%\FuncTests\FuncTests.exe
+Solutions\out\%CONFIGURATION%\%PLAT%\FuncTests\FuncTests.exe
 :end
