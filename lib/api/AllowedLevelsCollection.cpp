21c27e7b13679abfeda1a72118e341b94dfcb4ec
Max Golovanov <maxgolov@microsoft.com>
2019-09-17 17:19:15 -0700

    Move AllowedLevelsCollection from submodule into main repo. It greatly simplifies the build logistics: current implementation of LogManagerImpl requires that piece. Since it's very small / lean, it should be fine to keep it in the main. Keeping the rest of filtering functionality in a submodule.

---
+++ lib/api/AllowedLevelsCollection.cpp
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+#include "AllowedLevelsCollection.hpp"
+
+namespace ARIASDK_NS_BEGIN
+{
+    AllowedLevelsCollection::AllowedLevelsCollection(std::initializer_list<std::uint8_t>&& allowedLevels) noexcept
+        : m_allowedLevels(allowedLevels)
+    { }
+
+    bool AllowedLevelsCollection::IsLevelInCollection(std::uint8_t level) const noexcept
+    {
+        std::lock_guard<std::mutex> lock{ m_allowedLevelsLock };
+        return std::find(m_allowedLevels.cbegin(), m_allowedLevels.cend(), level) != m_allowedLevels.cend();
+    }
+
+    std::size_t AllowedLevelsCollection::GetSize() const noexcept
+    {
+        std::lock_guard<std::mutex> lock{ m_allowedLevelsLock };
+        return m_allowedLevels.size();
+    }
+
+    std::uint8_t AllowedLevelsCollection::operator[](int index) const noexcept
+    {
+        std::lock_guard<std::mutex> lock{ m_allowedLevelsLock };
+        return m_allowedLevels[index];
+    }
+
+    void AllowedLevelsCollection::UpdateAllowedLevels(const std::vector<std::uint8_t>& levels) noexcept
+    {
+        std::lock_guard<std::mutex> lock{ m_allowedLevelsLock };
+        m_allowedLevels = levels;
+    }
+
+} ARIASDK_NS_END
