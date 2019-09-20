34eedfc6455896d5b1a3811fabf7ae2e296c8257
kindbe <kindbe@comcast.net>
2019-09-19 20:30:24 -0700

    Add helpers to populate missing DeviceInfo fields on iOS (#116)

---
+++ lib/pal/posix/sysinfo_utils_ios.hpp
@@ -0,0 +1,11 @@
+#ifndef LIB_PAL_POSIX_SYSINFO_UTILS_IOS_HPP_
+#define LIB_PAL_POSIX_SYSINFO_UTILS_IOS_HPP_
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+#include <string>
+
+std::string get_device_model();
+
+std::string get_device_id();
+
+#endif /* LIB_PAL_POSIX_SYSINFO_UTILS_IOS_HPP_ */
