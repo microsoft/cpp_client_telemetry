45081626539b3925cfdbe1abff13b67daa091d4d
David Brown <dabrow@otelcet-macmini.guest.corp.microsoft.com>
2019-10-02 14:05:45 -0700

    Refactor Tests.mm and update copyright

--- tests/unittests/Tests/Tests.mm
+++ tests/unittests/Tests/Tests.mm
@@ -1,10 +1,4 @@
-//
-//  iOSTestAppTests.m
-//  iOSTestAppTests
-//
-//  Created by David Brown on 9/26/19.
-//  Copyright © 2019 David Brown. All rights reserved.
-//
+// Copyright (c) Microsoft. All rights reserved.
 
 #import <XCTest/XCTest.h>
 
@@ -44,25 +38,17 @@ private:
 @implementation iOSTestAppTests
 
 - (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-}
-
-- (void)testAll {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-    NSArray *arguments = [[NSProcessInfo processInfo] arguments];
-    int i = 0;
-    int argc = (int)[arguments count];
-    const char **argv = (const char **)calloc((unsigned int)argc + 1, sizeof(const char *));
-    for (NSString *arg in arguments) {
-        argv[i++] = [arg UTF8String];
-    }
+    NSArray *args = [[NSProcessInfo processInfo] arguments];
     
-    ::testing::InitGoogleMock(&argc, (char **)argv);
+    int argc = static_cast<int>([args count]);
+    std::vector<char*> argVector;
+    for (NSString *arg in args) {
+        char value[static_cast<size_t>([arg length])];
+        strcpy(value, [arg UTF8String]);
+        argVector.push_back(value);
+    }
+
+    ::testing::InitGoogleMock(&argc, argVector.data());
     TestStatusLogger* logger = new TestStatusLogger(self);
     auto googletest = ::testing::UnitTest::GetInstance();
     googletest->listeners().Append(logger);
@@ -70,8 +56,14 @@ private:
     ILogConfiguration logConfig;
     RuntimeConfig_Default runtimeConfig(logConfig);
     PAL::initialize(runtimeConfig);
-    (void)RUN_ALL_TESTS();
+}
+
+- (void)tearDown {
     PAL::shutdown();
+}
+
+- (void)testAll {
+    (void)RUN_ALL_TESTS();
 }
 
 @end
