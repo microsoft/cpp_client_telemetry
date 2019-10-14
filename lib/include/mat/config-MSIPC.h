a013b2d57e13833f98327d626e6eb2e21347631c
Vitaliy Gerasimov <viherasi@microsoft.com>
2019-10-14 15:59:45 -0700

    Fix for APIScan

--- lib/include/mat/config-MSIPC.h
+++ lib/include/mat/config-MSIPC.h
@@ -10,6 +10,7 @@
 #define HAVE_MAT_ZLIB
 #define HAVE_MAT_LOGGING
 #define HAVE_MAT_STORAGE
+#define WIN_DESKTOP
 /* #define HAVE_MAT_DEFAULT_HTTP_CLIENT */
 #if defined(_WIN32) && !defined(_WINRT_DLL)
 //define HAVE_MAT_NETDETECT
