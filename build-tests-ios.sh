81445e5335240966c9299cb3e18eeeea1235169b
David Brown <dabrow@otelcet-macmini.guest.corp.microsoft.com>
2019-10-14 16:10:21 -0700

    Try again

--- build-tests-ios.sh
+++ build-tests-ios.sh
@@ -4,7 +4,7 @@ SKU=${1:-release}
 SIMULATOR=${2:-iPhone 8}
 
 set -e
-build-ios.sh ${SKU}
+./build-ios.sh ${SKU}
 
 cd tests/unittests
 xcodebuild test -scheme iOSUnitTests -destination "platform=iOS Simulator,name=$SIMULATOR"
