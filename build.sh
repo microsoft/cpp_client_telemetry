b0435f850d6d590696bac2cc9587c092db2b69a6
Max Golovanov <maxgolov@microsoft.com>
2020-01-07 23:12:01 -0800

    Initial commit of changes to simplify creation of shared library

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
 export PATH=/usr/local/bin:$PATH
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
 
 if [[ ! -z "${GIT_PULL_TOKEN}" ]]; then
   rm -rf lib/modules
@@ -12,27 +13,59 @@ if [[ ! -z "${GIT_PULL_TOKEN}" ]]; then
   git clone https://${GIT_PULL_TOKEN}:x-oauth-basic@github.com/microsoft/cpp_client_telemetry_modules.git lib/modules
 fi
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "Current directory: $DIR"
-cd $DIR
-
 export NOROOT=$NOROOT
 
 if [ "$1" == "clean" ]; then
- rm -f CMakeCache.txt *.cmake
- rm -rf out
- rm -rf .buildtools
-# make clean
+  rm -f CMakeCache.txt *.cmake
+  rm -rf out
+  rm -rf .buildtools
+  # make clean
 fi
 
+# Don't elevate, don't deploy package
 if [ "$1" == "noroot" ] || [ "$2" == "noroot" ]; then
-export NOROOT=true
+  export NOROOT=true
 fi
 
+# Build release bits
 if [ "$1" == "release" ] || [ "$2" == "release" ]; then
-BUILD_TYPE="Release"
+  BUILD_TYPE="Release"
 else
-BUILD_TYPE="Debug"
+  BUILD_TYPE="Debug"
+fi
+
+LINK_TYPE=
+CMAKE_OPTS="${CMAKE_OPTS:-DBUILD_SHARED_LIBS=OFF}"
+while getopts "h?vl:D:" opt; do
+    case "$opt" in
+    h|\?)
+        echo "Usage: build.sh [clean] [noroot] [release] [-h|-?] [-l (static|shared)] [-D CMAKE_OPTION]"
+        echo "                                                                                         "
+        echo "options:                                                                                 "
+        echo "                                                                                         "
+        echo " -h | -?             - this help.                                                        "
+        echo " -l [static|shared]  - build static (default) or shared library.                         "
+        echo " -D [CMAKE_OPTION]   - additional option to pass to cmake.                               "
+        echo "                                                                                         "
+        echo "cmake options can be passed using CMAKE_OPTS environment variable.                       "
+        echo "                                                                                         "
+        exit 0
+        ;;
+    :)  echo "Invalid option: $OPTARG requires an argument" 1>&2
+        exit 0
+        ;;
+    v)  verbose=1
+        ;;
+    D)  CMAKE_OPTS="$CMAKE_OPTS -D$OPTARG"
+        ;;
+    l)  LINK_TYPE=$OPTARG
+        ;;
+    esac
+done
+shift $((OPTIND -1))
+
+if [ "$LINK_TYPE" == "shared" ]; then
+  CMAKE_OPTS="$CMAKE_OPTS -DBUILD_SHARED_LIBS=ON"
 fi
 
 # Set target MacOS minver
@@ -42,21 +75,21 @@ export MACOSX_DEPLOYMENT_TARGET=10.10
 FILE=.buildtools
 OS_NAME=`uname -a`
 if [ ! -f $FILE ]; then
-case "$OS_NAME" in
- *Darwin*) tools/setup-buildtools-mac.sh ;;
- *Linux*)  [[ -z "$NOROOT" ]] && sudo tools/setup-buildtools.sh || echo "No root: skipping build tools installation." ;;
- *)        echo "WARNING: unsupported OS $OS_NAME , skipping build tools installation.."
-esac
-# Assume that the build tools have been successfully installed
-echo > $FILE
+  case "$OS_NAME" in
+    *Darwin*) tools/setup-buildtools-mac.sh ;;
+     *Linux*) [[ -z "$NOROOT" ]] && sudo tools/setup-buildtools.sh || echo "No root: skipping build tools installation." ;;
+           *) echo "WARNING: unsupported OS $OS_NAME , skipping build tools installation." ;;
+  esac
+  # Assume that the build tools have been successfully installed
+  echo > $FILE
 fi
 
 if [ -f /usr/bin/gcc ]; then
-echo "gcc   version: `gcc --version`"
+  echo "gcc   version: `gcc --version`"
 fi
 
 if [ -f /usr/bin/clang ]; then
-echo "clang version: `clang --version`"
+  echo "clang version: `clang --version`"
 fi
 
 # Skip Version.hpp changes
@@ -71,21 +104,19 @@ CMAKE_PACKAGE_TYPE=tgz
 
 # .deb package
 if [ -f /usr/bin/dpkg ]; then
-export CMAKE_PACKAGE_TYPE=deb
+  export CMAKE_PACKAGE_TYPE=deb
 fi
 
 # .rpm package
 if [ -f /usr/bin/rpmbuild ]; then
-export CMAKE_PACKAGE_TYPE=rpm
+  export CMAKE_PACKAGE_TYPE=rpm
 fi
-
 
 # Fail on error
 set -e
 
-
 # TODO: pass custom build flags?
-cmake -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_PACKAGE_TYPE=$CMAKE_PACKAGE_TYPE ..
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_PACKAGE_TYPE=$CMAKE_PACKAGE_TYPE $CMAKE_OPTS ..
 # TODO: strip symbols to minimize (release-only)
 
 # Build all
@@ -104,13 +135,13 @@ make package
 
 # Debian / Ubuntu / Raspbian
 if [ -f /usr/bin/dpkg ]; then
-# Install new package
-[[ -z "$NOROOT" ]] && sudo dpkg -i *.deb || echo "No root: skipping package deployment."
+  # Install new package
+  [[ -z "$NOROOT" ]] && sudo dpkg -i *.deb || echo "No root: skipping package deployment."
 fi
 
 # RedHat / CentOS
 if [ -f /usr/bin/rpmbuild ]; then
-[[ -z "$NOROOT" ]] && sudo rpm -i --force -v *.rpm || echo "No root: skipping package deployment."
+  [[ -z "$NOROOT" ]] && sudo rpm -i --force -v *.rpm || echo "No root: skipping package deployment."
 fi
 
 # Install SDK headers and lib to /usr/local
@@ -120,10 +151,10 @@ fi
 ## strip -S --strip-unneeded --remove-section=.note.gnu.gold-version --remove-section=.comment --remove-section=.note --remove-section=.note.gnu.build-id --remove-section=.note.ABI-tag out/lib/libmat.so
 
 if [ "$CMAKE_PACKAGE_TYPE" == "tgz" ]; then
-cd ..
-MATSDK_INSTALL_DIR="${MATSDK_INSTALL_DIR:-/usr/local}"
-echo "+-----------------------------------------------------------------------------------+"
-echo " This step may prompt for your sudo password to deploy SDK to $MATSDK_INSTALL_DIR  "
-echo "+-----------------------------------------------------------------------------------+"
-[[ -z "$NOROOT" ]] && sudo ./install.sh $MATSDK_INSTALL_DIR || echo "No root: skipping package deployment."
+  cd ..
+  MATSDK_INSTALL_DIR="${MATSDK_INSTALL_DIR:-/usr/local}"
+  echo "+-----------------------------------------------------------------------------------+"
+  echo " This step may prompt for your sudo password to deploy SDK to $MATSDK_INSTALL_DIR  "
+  echo "+-----------------------------------------------------------------------------------+"
+  [[ -z "$NOROOT" ]] && sudo ./install.sh $MATSDK_INSTALL_DIR || echo "No root: skipping package deployment."
 fi
