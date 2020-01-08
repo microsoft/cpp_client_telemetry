721704c23f8c517ad541a30669b48c77e6ddf53e
Max Golovanov <maxgolov@microsoft.com>
2020-01-08 11:33:06 -0800

    Win 10 UWP min C++ build that turns off Offline Storage and ECS

---
+++ lib/include/mat/config-compact-min.h
@@ -0,0 +1,13 @@
+#define EVTSDK_VERSION_PREFIX "EVT"
+#if defined(_WIN32)
+#define HAVE_MAT_UTC
+#endif
+/* #define HAVE_MAT_EXP        */
+#define HAVE_MAT_FIFOSTORAGE
+#define HAVE_MAT_JSONHPP
+/* #define HAVE_MAT_ZLIB       */
+/* #define HAVE_MAT_LOGGING    */
+/* #define HAVE_MAT_STORAGE    */
+/* #define HAVE_MAT_NETDETECT  */
+/* #define HAVE_MAT_SHORT_NS   */
+#define HAVE_MAT_DEFAULT_HTTP_CLIENT
