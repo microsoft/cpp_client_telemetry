0c7346ad4d50973e92623074397f332ba4487616
Max Golovanov <maxgolov@microsoft.com>
2020-01-18 18:32:07 -0800

    Working implementation of C# .NET Core 3.1 wrapper on top of C API (tested on Mac)

--- wrappers/netcore/EventNativeAPI.cs
+++ wrappers/netcore/EventNativeAPI.cs
@@ -1,5 +1,6 @@
 #pragma warning disable IDE1006 // ignore naming rule violations: we preserve original C API naming for clarity here
 #pragma warning disable IDE0044 // ignore readonly suggestion for field passed over P/Invoke
+#undef TRACE
 
 using System;
 using System.Runtime.InteropServices;
@@ -23,7 +24,7 @@ namespace Microsoft
                 public const string VERSION = "3.3.0-netcore";
             }
 
-            public enum EventCallType
+            public enum EventCallType : UInt32
             {
                 EVT_OP_LOAD = 0x00000001,
                 EVT_OP_UNLOAD = 0x00000002,
@@ -40,7 +41,7 @@ namespace Microsoft
                 EVT_OP_MAX = EVT_OP_OPEN_WITH_PARAMS + 1
             }
 
-            public enum EventPropertyType
+            public enum EventPropertyType : UInt32
             {
                 /* Basic types */
                 TYPE_STRING = 0,
@@ -60,18 +61,38 @@ namespace Microsoft
                 TYPE_NULL = 12
             }
 
-            public struct PiiValue
+            public struct PiiKind
             {
-                public string value;
-                public UInt32 piKind;
+                /// <summary>No PII kind.</summary>
+                public const int None = 0;
+                /// <summary>An LDAP distinguished name.</summary>
+                public const int DistinguishedName = 1;
+                /// <summary>Generic data.</summary>
+                public const int GenericData = 2;
+                /// <summary>An IPV4 Internet address.</summary>
+                public const int IPv4Address = 3;
+                /// <summary>An IPV6 Internet address.</summary>
+                public const int IPv6Address = 4;
+                /// <summary>An e-mail subject.</summary>
+                public const int MailSubject = 5;
+                /// <summary>A telephone number.</summary>
+                public const int PhoneNumber = 6;
+                /// <summary>A query string.</summary>
+                public const int QueryString = 7;
+                /// <summary>A SIP address</summary>
+                public const int SipAddress = 8;
+                /// <summary>An e-mail address.</summary>
+                public const int SmtpAddress = 9;
+                /// <summary>An identity.</summary>
+                public const int Identity = 10;
+                /// <summary>A uniform resource indicator.</summary>
+                public const int Uri = 11;
+                /// <summary>A fully-qualified domain name.</summary>
+                public const int Fqdn = 12;
+                /// <summary>A legacy IPV4 Internet address.</summary>
+                public const int IPv4AddressLegacy = 13;
+                public const int CustomerData = 32;
             }
-
-            public class EventProperties : Dictionary<string, object>
-            {
-                public EventProperties()
-                {
-                }
-            };
 
             [StructLayout(LayoutKind.Explicit, Size = 16, CharSet = CharSet.Ansi)]
             public unsafe struct EventGUIDType
@@ -106,14 +127,14 @@ namespace Microsoft
                 fixed byte Data4[8];
             }
 
-            [StructLayout(LayoutKind.Explicit, Size = 24, CharSet = CharSet.Ansi)]
-            public struct EventContextType
+            [StructLayout(LayoutKind.Explicit, Size = 28, CharSet = CharSet.Ansi)]
+            public unsafe struct EventContextType
             {
                 [FieldOffset(0)] public UInt32 call;
                 [FieldOffset(4)] public ulong handle;
                 [FieldOffset(12)] public IntPtr data;
-                [FieldOffset(16)] public UInt32 result;
-                [FieldOffset(20)] public UInt32 size;
+                [FieldOffset(20)] public UInt32 result;
+                [FieldOffset(24)] public UInt32 size;
             }
 
             public enum EventOpenParamType
@@ -126,10 +147,33 @@ namespace Microsoft
             }
 
             [StructLayout(LayoutKind.Explicit, Size = 12, CharSet = CharSet.Ansi)]
-            public struct EventOpenParam
+            public unsafe struct EventOpenParam
             {
                 [FieldOffset(0)] public UInt32 type;
                 [FieldOffset(4)] public IntPtr data;
+            };
+
+            [StructLayout(LayoutKind.Explicit, Size = 8, CharSet = CharSet.Ansi)]
+            public unsafe struct EventPropertyValue
+            {
+                /* Basic types */
+                [FieldOffset(0)] public UInt64 as_uint64;
+                [FieldOffset(0)] public IntPtr as_string;
+                [FieldOffset(0)] public Int64  as_int64;
+                [FieldOffset(0)] public double as_double;
+                [FieldOffset(0)] public bool   as_bool;
+                [FieldOffset(0)] public IntPtr as_guid;
+                [FieldOffset(0)] public UInt64 as_time;
+#if FALSE
+                /* We don't support passing arrays yet. Code below needs to be ported from C++ to C# */
+                /* Array types are nullptr-terminated array of pointers */
+                char**              as_arr_string;
+                int64_t**           as_arr_int64;
+                bool**              as_arr_bool;
+                double**            as_arr_double;
+                evt_guid_t**        as_arr_guid;
+                uint64_t**          as_arr_time;
+#endif
             };
 
             /**
@@ -146,43 +190,20 @@ namespace Microsoft
             // custom SDK build flag that enforces certain struct layout. i.e.
             // positioning the two pointers below as 64-bit integers instead of 32-bit.
             [StructLayout(LayoutKind.Explicit, Size = 20, CharSet = CharSet.Ansi)]
-            public struct EventOpenWithParamsDataType
+            public unsafe struct EventOpenWithParamsDataType
             {
                 [FieldOffset(0)] public IntPtr config;
                 [FieldOffset(8)] public IntPtr parameters; /* pointer to array of EventOpenParam */
                 [FieldOffset(8)] public UInt32 paramsCount;
             }
 
-            [StructLayout(LayoutKind.Explicit, Size = 8, CharSet = CharSet.Ansi)]
-            public struct EventPropertyValue
+            [StructLayout(LayoutKind.Explicit, Size = 24, CharSet = CharSet.Ansi)]
+            public unsafe struct EventPropertyKeyValue
             {
-                /* Basic types */
-                [FieldOffset(0)] UInt64 as_uint64;
-                [FieldOffset(0)] IntPtr as_string;
-                [FieldOffset(0)] Int64 as_int64;
-                [FieldOffset(0)] double as_double;
-                [FieldOffset(0)] bool as_bool;
-                [FieldOffset(0)] IntPtr as_guid;
-                [FieldOffset(0)] UInt64 as_time;
-#if FALSE
-                /* We don't support passing arrays yet. Code below needs to be ported from C++ to C# */
-                /* Array types are nullptr-terminated array of pointers */
-                char**              as_arr_string;
-                int64_t**           as_arr_int64;
-                bool**              as_arr_bool;
-                double**            as_arr_double;
-                evt_guid_t**        as_arr_guid;
-                uint64_t**          as_arr_time;
-#endif
-            };
-
-            [StructLayout(LayoutKind.Explicit, Size = 16, CharSet = CharSet.Ansi)]
-            public struct EventProperty
-            {
-                [FieldOffset(0)] IntPtr name;
-                [FieldOffset(4)] EventPropertyType type;
-                [FieldOffset(8)] EventPropertyValue value;
-                [FieldOffset(12)] UInt32 piiKind;
+                [FieldOffset(0)] public IntPtr name;
+                [FieldOffset(8)] public EventPropertyType type;
+                [FieldOffset(12)] public EventPropertyValue value;
+                [FieldOffset(20)] public UInt32 piiKind;
             }
 
             /**
@@ -195,6 +216,174 @@ namespace Microsoft
                 HTTP_REQUEST_TYPE_GET = 0,
                 HTTP_REQUEST_TYPE_POST = 1,
             }
+
+            public class EventProperties : Dictionary<string, EventProperty>
+            {
+
+                public IntPtr nativeBuffer = IntPtr.Zero;
+                public int nativeSize = 0;
+                public int szEvtPropKV = Marshal.SizeOf(typeof(EventPropertyKeyValue));
+
+                public EventProperties()
+                {
+                }
+                internal unsafe void AllocNative()
+                {
+                    nativeSize = (Count+1) * szEvtPropKV;
+                    nativeBuffer = Marshal.AllocHGlobal(nativeSize);// sizeof(EventPropertyKeyValue));
+                    int i = 0;
+                    EventPropertyKeyValue* propPtr = (EventPropertyKeyValue*)(IntPtr.Zero);
+                    foreach (KeyValuePair<string, EventProperty> item in this)
+                    {
+                        propPtr = (EventPropertyKeyValue*)(nativeBuffer) + i;
+                        (*propPtr).name = Marshal.StringToHGlobalAnsi(item.Key);
+                        (*propPtr).piiKind = 0; // TODO: add Pii Kind support
+                        (*propPtr).type = item.Value.type;
+#if (TRACE)
+                        Console.Write("0x{0:X} ", (long)propPtr);
+#endif
+                        switch (item.Value.type)
+                        {
+                            case EventPropertyType.TYPE_BOOLEAN:
+                                (*propPtr).value.as_bool = item.Value.value.as_bool;
+#if (TRACE)
+                                Console.WriteLine("boolean {0}={1}", item.Key, (*propPtr).value.as_bool);
+#endif
+                                break;
+                            case EventPropertyType.TYPE_DOUBLE:
+                                (*propPtr).value.as_double = item.Value.value.as_double;
+#if (TRACE)
+                                Console.WriteLine("double {0}={1}", item.Key, (*propPtr).value.as_double);
+#endif
+                                break;
+                            case EventPropertyType.TYPE_GUID:
+                                // TODO: not implemented
+#if (TRACE)
+                                Console.WriteLine("guid    {0}={1}", item.Key, (*propPtr).value.as_guid);
+#endif
+                                break;
+                            case EventPropertyType.TYPE_INT64:
+                                (*propPtr).value.as_int64 = item.Value.value.as_int64;
+#if (TRACE)
+                                Console.WriteLine("int64  {0}={1}", item.Key, (*propPtr).value.as_int64);
+#endif
+                                break;
+                            case EventPropertyType.TYPE_STRING:
+                                {
+                                    string s = (string)(item.Value.objValue);
+                                    (*propPtr).value.as_string = Marshal.StringToHGlobalAnsi(s);
+                                    (*propPtr).piiKind = item.Value.piiKind;
+#if (TRACE)
+                                    Console.WriteLine("string {0}={1} (piiKind={2})", item.Key, s, item.Value.piiKind);
+#endif
+                                    break;
+                                }
+                            default:
+                                break;
+                        }
+                        i++;
+                    }
+                    /* NULL terminator property at the end of property list */
+                    propPtr = (EventPropertyKeyValue*)(nativeBuffer) + i;
+                    (*propPtr).name = IntPtr.Zero;
+                    (*propPtr).type = EventPropertyType.TYPE_NULL;
+                }
+
+                internal unsafe void FreeNative()
+                {
+                    if (nativeBuffer==IntPtr.Zero)
+                    {
+                        throw new Exception("Memory not allocated!");
+                    }
+                    // TODO: assert count==Count
+                    int count = nativeSize / szEvtPropKV;
+                    for (int i = 0; i < count; i++)
+                    {
+                        EventPropertyKeyValue* propPtr = (EventPropertyKeyValue*)(nativeBuffer) + i;
+                        EventPropertyType type = (EventPropertyType)((*propPtr).type);
+                        switch (type)
+                        {
+                            case EventPropertyType.TYPE_GUID:
+                                // TODO: not implemented
+                                break;
+                            case EventPropertyType.TYPE_STRING:
+                                {
+                                    IntPtr strPtr = (*propPtr).value.as_string;
+                                    if (strPtr != IntPtr.Zero)
+                                    {
+                                        Marshal.FreeHGlobal(strPtr);
+                                    }
+                                    break;
+                                }
+                            default:
+                                break;
+                        }
+                        if ((*propPtr).name != IntPtr.Zero)
+                        {
+                            Marshal.FreeHGlobal((*propPtr).name);
+                        }
+                    }
+                    Marshal.FreeHGlobal(nativeBuffer);
+                    nativeBuffer = IntPtr.Zero;
+                    nativeSize = 0;
+                }
+            }
+
+            public class EventProperty
+            {
+                public EventPropertyType type;
+                public EventPropertyValue value;
+                public object objValue = null;
+                public UInt32 piiKind = 0;
+
+                public EventProperty(string strValue)
+                {
+                    type = EventPropertyType.TYPE_STRING;
+                    objValue = strValue;
+                }
+
+                public EventProperty(string strValue, UInt32 piiKind)
+                {
+                    type = EventPropertyType.TYPE_STRING;
+                    objValue = strValue;
+                    this.piiKind = piiKind;
+                }
+
+                public EventProperty(Guid guidValue)
+                {
+                    type = EventPropertyType.TYPE_GUID;
+                    objValue = guidValue;
+                }
+
+                public EventProperty(Int64 intValue)
+                {
+                    type = EventPropertyType.TYPE_INT64;
+                    value.as_int64 = intValue;
+                }
+
+                public EventProperty(int intValue)
+                {
+                    type = EventPropertyType.TYPE_INT64;
+                    value.as_int64 = intValue;
+                }
+
+                public EventProperty(double doubleValue)
+                {
+                    type = EventPropertyType.TYPE_DOUBLE;
+                    value.as_double = doubleValue;
+                }
+
+                public EventProperty(bool boolValue)
+                {
+                    type = EventPropertyType.TYPE_BOOLEAN;
+                    value.as_bool = boolValue;
+                }
+
+                public static implicit operator EventProperty(string v) => new EventProperty(v);
+                public static implicit operator EventProperty(int v) => new EventProperty(v);
+                public static implicit operator EventProperty(double v) => new EventProperty(v);
+
+            };
 
             public static class EventNativeAPI
             {
@@ -218,6 +407,7 @@ namespace Microsoft
                         data = Marshal.StringToHGlobalAnsi(cfg)
                     };
                     evt_api_call(ref context);
+                    Marshal.FreeHGlobal(context.data);
                     return context.handle;
                 }
 
@@ -240,12 +430,21 @@ namespace Microsoft
 
                 public static ulong evt_log(ulong inHandle, ref EventProperties properties)
                 {
-                    EventContextType context = new EventContextType
+                    ulong result = 0;
+                    unsafe
                     {
-                        call = (Byte)EventCallType.EVT_OP_LOG,
-                        handle = inHandle
+                        properties.AllocNative();
+                        EventContextType context = new EventContextType
+                        {
+                            call = (Byte)EventCallType.EVT_OP_LOG,
+                            handle = inHandle,
+                            data = properties.nativeBuffer,
+                            size = 0 /* (uint)(properties.Count) */
+                        };
+                        result = evt_api_call(ref context);
+                        properties.FreeNative();
                     };
-                    return evt_api_call(ref context);
+                    return result;
                 }
 
                 /**
@@ -337,7 +536,9 @@ namespace Microsoft
                         data = nativeDataPtr
                     };
                     evt_api_call(ref context);
-                    return Marshal.PtrToStringAnsi(context.data);
+                    string result = Marshal.PtrToStringAnsi(context.data);
+                    Marshal.FreeHGlobal(nativeDataPtr);
+                    return result;
                 }
             }
 
--- wrappers/netcore/Program.cs
+++ wrappers/netcore/Program.cs
@@ -3,6 +3,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.IO;
 using System.Json;
+using System.Diagnostics;
 
 using Microsoft.Telemetry.Core;
 
@@ -35,22 +36,45 @@ namespace EventSender
             Console.WriteLine("SDK version: {0}", EventNativeAPI.evt_version());
 
             // Initialize
-            Console.WriteLine("evt_open...");
+            Console.WriteLine(">>> evt_open...");
             var handle = EventNativeAPI.evt_open(cfg);
             Console.WriteLine("handle={0}", handle);
 
-#if FALSE
-            var props = new EventProperties() {
-                { "strKey", "value1" },
-                { "intKey", 12345 },
-                { "dblKey", 0.12345 },
-                { "guidKey", new Guid("73e21739-9d4e-497d-9c66-8e399a532ec9") }
-            };
-            EventNativeAPI.evt_log(handle, ref props);
-#endif
+            // Initialize
+            Console.WriteLine(">>> evt_pause...");
+            EventNativeAPI.evt_pause(handle);
+
+            Console.WriteLine(">>> evt_log...");
+
+            long total0 = GC.GetTotalMemory(true);
+            long frag0  = GC.GetGCMemoryInfo().FragmentedBytes;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            const int MAX_ITERATIONS = 100000;
+            for (int i = 0; i < MAX_ITERATIONS; i++)
+            {
+                var props = new EventProperties() {
+                    { "strKey", "value1" },
+                    { "intKey", 12345 },
+                    { "dblKey", 0.12345 }
+//                  , { "guidKey", new Guid("73e21739-9d4e-497d-9c66-8e399a532ec9") }
+                };
+                EventNativeAPI.evt_log(handle, ref props);
+            }
+            sw.Stop();
+            long total1 = GC.GetTotalMemory(true);
+            long frag1  = GC.GetGCMemoryInfo().FragmentedBytes;
+            // Print some benchmarking results for offline storage + serialization
+            TimeSpan ts = sw.Elapsed;
+            Console.WriteLine("Elapsed    = {0}", ts);
+            Console.WriteLine("Event rate = {0} eps", (MAX_ITERATIONS/ts.TotalSeconds) );
+            Console.WriteLine("Latency    = {0} ms", (ts.TotalMilliseconds/MAX_ITERATIONS) );
+            Console.WriteLine("Mem used   = {0} bytes", total1-total0);
+            Console.WriteLine("Fragmented = {0} bytes", frag1-frag0);
 
             // FlushAndTeardown
-            Console.WriteLine("evt_close...");
+            Console.WriteLine(">>> evt_close...");
             var result = EventNativeAPI.evt_close(handle);
             Console.WriteLine("result={0}", result);
         }
