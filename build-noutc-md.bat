821885652f20e1ca048464028f6ccf78c8505624
Max Golovanov <maxgolov@microsoft.com>
2020-01-29 14:06:14 -0800

    Build no-UTC static library (/MD)

--- build-noutc-md.bat
+++ build-noutc-md.bat
@@ -3,10 +3,7 @@ cd %~dp0
 call tools\gen-version.cmd
 @setlocal ENABLEEXTENSIONS
 
-if DEFINED GIT_PULL_TOKEN (
-  rd /s /q lib\modules
-  git clone https://%GIT_PULL_TOKEN%:x-oauth-basic@github.com/microsoft/cpp_client_telemetry_modules.git lib\modules
-)
+rmdir /S /Q lib\modules
 
 echo "Building using Visual Studio 2017 tools"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
@@ -15,12 +12,10 @@ set MAXCPUCOUNT=%NUMBER_OF_PROCESSORS%
 set platform=
 set SOLUTION=Solutions\MSTelemetrySDK.sln
 
-set CUSTOM_PROPS=
-if ("%1"=="") goto skip
-set CUSTOM_PROPS="/p:ForceImportBeforeCppTargets=%1"
+REM set NOSQLITE=TRUE
+set CUSTOM_PROPS="/p:ForceImportBeforeCppTargets=%~dp0\Solutions\build.noutc.props"
 echo Using custom properties file for the build:
 echo %CUSTOM_PROPS%
-:skip
 
 REM DLL and static /MD build
 msbuild %SOLUTION% /target:sqlite,zlib,win32-lib /p:BuildProjectReferences=true /maxcpucount:%MAXCPUCOUNT% /p:Configuration=Debug /p:Platform=Win32 %CUSTOM_PROPS%
