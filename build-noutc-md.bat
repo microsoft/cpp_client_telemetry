7eea104a093cd2073ba221295130647f2eceb657
Michael Scott <michsco@microsoft.com>
2020-01-29 17:03:25 -0800

    Update with Minecraft requirements.

--- build-noutc-md.bat
+++ build-noutc-md.bat
@@ -22,3 +22,28 @@ msbuild %SOLUTION% /target:sqlite,zlib,win32-lib /p:BuildProjectReferences=true 
 msbuild %SOLUTION% /target:sqlite,zlib,win32-lib /p:BuildProjectReferences=true /maxcpucount:%MAXCPUCOUNT% /p:Configuration=Release /p:Platform=Win32 %CUSTOM_PROPS%
 msbuild %SOLUTION% /target:sqlite,zlib,win32-lib /p:BuildProjectReferences=true /maxcpucount:%MAXCPUCOUNT% /p:Configuration=Debug /p:Platform=x64 %CUSTOM_PROPS%
 msbuild %SOLUTION% /target:sqlite,zlib,win32-lib /p:BuildProjectReferences=true /maxcpucount:%MAXCPUCOUNT% /p:Configuration=Release /p:Platform=x64 %CUSTOM_PROPS%
+
+REM Consolidate the libraries with the right names.
+mkdir Solutions\out\Debug\Win32\libs
+copy Solutions\out\Debug\Win32\sqlite\sqlite.* Solutions\out\Debug\Win32\libs
+copy Solutions\out\Debug\Win32\win32-lib\ClientTelemetry.* Solutions\out\Debug\Win32\libs
+copy Solutions\out\Debug\Win32\zlib\zlib.lib Solutions\out\Debug\Win32\libs\oneds-zlib.lib
+copy Solutions\out\Debug\Win32\zlib\zlib.pdb Solutions\out\Debug\Win32\libs\oneds-zlib.pdb
+
+mkdir Solutions\out\Debug\x64\libs
+copy Solutions\out\Debug\x64\sqlite\sqlite.* Solutions\out\Debug\x64\libs
+copy Solutions\out\Debug\x64\win32-lib\ClientTelemetry.* Solutions\out\Debug\x64\libs
+copy Solutions\out\Debug\x64\zlib\zlib.lib Solutions\out\Debug\x64\libs\oneds-zlib.lib
+copy Solutions\out\Debug\x64\zlib\zlib.pdb Solutions\out\Debug\x64\libs\oneds-zlib.pdb
+
+mkdir Solutions\out\Release\Win32\libs
+copy Solutions\out\Release\Win32\sqlite\sqlite.* Solutions\out\Release\Win32\libs
+copy Solutions\out\Release\Win32\win32-lib\ClientTelemetry.* Solutions\out\Release\Win32\libs
+copy Solutions\out\Release\Win32\zlib\zlib.lib Solutions\out\Release\Win32\libs\oneds-zlib.lib
+copy Solutions\out\Release\Win32\zlib\zlib.pdb Solutions\out\Release\Win32\libs\oneds-zlib.pdb
+
+mkdir Solutions\out\Release\x64\libs
+copy Solutions\out\Release\x64\sqlite\sqlite.* Solutions\out\Release\x64\libs
+copy Solutions\out\Release\x64\win32-lib\ClientTelemetry.* Solutions\out\Release\x64\libs
+copy Solutions\out\Release\x64\zlib\zlib.lib Solutions\out\Release\x64\libs\oneds-zlib.lib
+copy Solutions\out\Release\x64\zlib\zlib.pdb Solutions\out\Release\x64\libs\oneds-zlib.pdb
