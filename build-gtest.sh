a6369770410fe2159619a0fe3df054129b491175
Max Golovanov <maxgolov@microsoft.com>
2020-05-02 11:46:33 -0700

    Update build-gtest.sh
    Use new Google Test on Ubuntu 20.04

--- build-gtest.sh
+++ build-gtest.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 cd `dirname $0`
+
+# Use latest Google Test
+if [ -f /etc/os-release ]; then
+  source /etc/os-release
+  # Use new Google Test on latest Ubuntu 20.04 : old one no longer compiles on 20
+  if [ "$VERSION_ID" == "20.04" ]; then
+    rm -rf googletest
+    git clone https://github.com/google/googletest
+  fi
+fi
+
 cd googletest
 set -evx
 env | sort
@@ -13,5 +24,5 @@ cmake -Dgtest_build_samples=ON \
       -DCMAKE_CXX_FLAGS="-fPIC $CXX_FLAGS" \
       ..
 make
-CTEST_OUTPUT_ON_FAILURE=1 make test
-#make install
+# CTEST_OUTPUT_ON_FAILURE=1 make test
+# make install
