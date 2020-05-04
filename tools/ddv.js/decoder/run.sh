559c0b0a95b9cf782eb76f122d81572a622f9d01
Max Golovanov <maxgolov@microsoft.com>
2020-05-04 02:36:02 -0700

    Refactor into separate Decoder module

--- tools/ddv.js/decoder/run.sh
+++ tools/ddv.js/decoder/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p sandbox
 # Invoke build-emcc.sh script from top-level directory
-( cd ../../; ./build-emcc.sh )
+( cd ../../../; ./build-emcc.sh )
 # Start node from current directory
 node --trace-warnings --experimental-wasm-bigint --experimental-wasm-threads --experimental-wasm-bulk-memory main.js
