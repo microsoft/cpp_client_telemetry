e26628c492ffdc2ec75b445076b890300455fa16
kindbe <kindbe@comcast.net>
2020-05-06 11:58:41 -0700

    Build gtest/gmock for iOS (#346)
    With Xcode 11.4, using mac libs in simulator tests is no longer allowed.  This change threads iOS options through the setup and gtest build scripts to output gtest/gmock libs for simulator.
    
    Co-authored-by: David Brown <dabrow@microsoft.com>

--- build-tests-ios.sh
+++ build-tests-ios.sh
@@ -6,12 +6,6 @@ SIMULATOR=${2:-iPhone 8}
 set -e
 ./build-ios.sh ${SKU}
 
-if [ -d "/Applications/Xcode_11.4.1.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/" ] 
-then
-  echo "Xcode 11.4.1 test build is not supported yet (see https://forums.developer.apple.com/thread/130684 )"
-  exit 0
-fi
-
 cd tests/unittests
 xcodebuild test -scheme iOSUnitTests -destination "platform=iOS Simulator,name=$SIMULATOR"
 
