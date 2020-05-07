ed3fd3fd6a0dfb125b29f426fa8704d7974394e5
Max Golovanov <maxgolov@microsoft.com>
2020-05-07 00:35:02 -0700

    More efficient file copy + detailed logging

--- examples/cpp/EventSender/deploy-dll.cmd
+++ examples/cpp/EventSender/deploy-dll.cmd
@@ -2,12 +2,12 @@
 set PROJECT_DIR=%~dp0
 
 @mkdir %PROJECT_DIR%\include 2> NUL
-copy /Y %PROJECT_DIR%..\..\..\lib\include\public\*.* %PROJECT_DIR%\include
+robocopy %PROJECT_DIR%..\..\..\lib\include\public\ %PROJECT_DIR%\include
 
 @mkdir %PROJECT_DIR%\lib\%1\%2 2> NUL
-copy /Y %PROJECT_DIR%..\..\..\Solutions\out\%1\%2\win32-dll\*.lib %PROJECT_DIR%\lib\%1\%2
+robocopy %PROJECT_DIR%..\..\..\Solutions\out\%1\%2\win32-dll\ %PROJECT_DIR%\lib\%1\%2 "*.lib""
 
 @mkdir %PROJECT_DIR%\%1\%2 2> NUL
-copy /Y %PROJECT_DIR%..\..\..\Solutions\out\%1\%2\win32-dll\*.* %PROJECT_DIR%\lib\%1\%2
-copy /Y %PROJECT_DIR%..\..\..\Solutions\out\%1\%2\win32-dll\*.* %3
+robocopy %PROJECT_DIR%..\..\..\Solutions\out\%1\%2\win32-dll\ %PROJECT_DIR%\lib\%1\%2
+robocopy %PROJECT_DIR%..\..\..\Solutions\out\%1\%2\win32-dll\ %3
 exit /b 0
