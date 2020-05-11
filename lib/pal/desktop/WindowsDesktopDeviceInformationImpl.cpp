cec051c379b7e6eb0809542fde70f207805e49bc
Matt Jaeger <majaeger@microsoft.com>
2020-05-11 10:46:45 -0700

    Fix another unreference parameter warning

--- lib/pal/desktop/WindowsDesktopDeviceInformationImpl.cpp
+++ lib/pal/desktop/WindowsDesktopDeviceInformationImpl.cpp
@@ -109,7 +109,7 @@ namespace PAL_NS_BEGIN {
     }
 
     ///// IDeviceInformation API
-    DeviceInformationImpl::DeviceInformationImpl(MAT::IRuntimeConfig& configuration) :
+    DeviceInformationImpl::DeviceInformationImpl(MAT::IRuntimeConfig& /*configuration*/) :
             m_registeredCount(0),
             m_info_helper()
     {
