95d8d3dbbde559a6ba6d18e6e7285444c400f32c
David Brown <dabrow@microsoft.com>
2020-05-12 14:26:40 -0700

    Add ODWSemanticContext with setAppId method

---
+++ wrappers/obj-c/ODWSemanticContext_private.h
@@ -0,0 +1,22 @@
+#include "objc_begin.h"
+#include "ISemanticContext.hpp"
+#import "ODWSemanticContext.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+using namespace MAT;
+
+/*!
+ The <b>ODWSemanticContext</b> class represents semantic context to be applied to events.
+*/
+@interface ODWSemanticContext (Private)
+
+/*!
+ @brief Constructs an ODWSemanticContext  object, taking internal API semantic context pointer. This method might be only used internally by wrapper.
+ */
+-(instancetype)initWithISemanticContext:(ISemanticContext *)context;
+
+@end
+
+NS_ASSUME_NONNULL_END
+#include "objc_end.h"
