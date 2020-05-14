42df0384a31424d4837d555e8775366b3710af83
Matt Koscumb <makoscum@microsoft.com>
2020-05-14 10:08:19 -0700

    Add constructors for common rule patterns (as observed in the default rules).

--- tests/unittests/TransmitProfileRuleTests.cpp
+++ tests/unittests/TransmitProfileRuleTests.cpp
@@ -34,3 +34,57 @@ TEST(TransmitProfileRuleTests, DefaultConstructor_TimersSizeIsZero)
     TransmitProfileRule rule;
     ASSERT_EQ(rule.timers.size(), size_t{0});
 }
+
+TEST(TransmitProfileRuleTests, TimerConstructor_TimersSizeThree_TimersSet)
+{
+    TransmitProfileRule rule{{1, 2, 3}};
+    ASSERT_EQ(rule.timers.size(), size_t{3});
+    ASSERT_EQ(rule.timers[0], 1);
+    ASSERT_EQ(rule.timers[1], 2);
+    ASSERT_EQ(rule.timers[2], 3);
+}
+
+TEST(TransmitProfileRuleTests, TimerConstructor_TimersSizeThree_OtherValuesUnmodified)
+{
+    TransmitProfileRule rule{{1, 2, 3}};
+    ASSERT_EQ(rule.netCost, NetworkCost::NetworkCost_Any);
+    ASSERT_EQ(rule.powerState, PowerSource::PowerSource_Any);
+    ASSERT_EQ(rule.netType, NetworkType::NetworkType_Any);
+    ASSERT_EQ(rule.netSpeed, unsigned{0});
+}
+
+TEST(TransmitProfileRuleTests, NetCostAndTimerConstructor_NetCostLowTimersSizeThree_NetCostAndTimersSet)
+{
+    TransmitProfileRule rule{NetworkCost::NetworkCost_Unmetered, {1, 2, 3}};
+    ASSERT_EQ(rule.netCost, NetworkCost::NetworkCost_Unmetered);
+    ASSERT_EQ(rule.timers.size(), size_t{3});
+    ASSERT_EQ(rule.timers[0], 1);
+    ASSERT_EQ(rule.timers[1], 2);
+    ASSERT_EQ(rule.timers[2], 3);
+}
+
+TEST(TransmitProfileRuleTests, NetCostAndTimerConstructor_NetCostLowTimersSizeThree_OtherValuesUnmodified)
+{
+    TransmitProfileRule rule{NetworkCost::NetworkCost_Unmetered, {1, 2, 3}};
+    ASSERT_EQ(rule.powerState, PowerSource::PowerSource_Any);
+    ASSERT_EQ(rule.netType, NetworkType::NetworkType_Any);
+    ASSERT_EQ(rule.netSpeed, unsigned{0});
+}
+
+TEST(TransmitProfileRuleTests, NetCostPowerSourceAndTimerConstructor_NetCostLowPowerSourceBatteryTimersSizeThree_NetCostPowerSourceAndTimersSet)
+{
+    TransmitProfileRule rule{NetworkCost::NetworkCost_Unmetered, PowerSource::PowerSource_Battery, {1, 2, 3}};
+    ASSERT_EQ(rule.netCost, NetworkCost::NetworkCost_Unmetered);
+    ASSERT_EQ(rule.powerState, PowerSource::PowerSource_Battery);
+    ASSERT_EQ(rule.timers.size(), size_t{3});
+    ASSERT_EQ(rule.timers[0], 1);
+    ASSERT_EQ(rule.timers[1], 2);
+    ASSERT_EQ(rule.timers[2], 3);
+}
+
+TEST(TransmitProfileRuleTests, NetCostPowerSourceAndTimerConstructor_NetCostLowPowerSourceBatteryTimersSizeThree_OtherValuesUnmodified)
+{
+    TransmitProfileRule rule{NetworkCost::NetworkCost_Unmetered, PowerSource::PowerSource_Battery, {1, 2, 3}};
+    ASSERT_EQ(rule.netType, NetworkType::NetworkType_Any);
+    ASSERT_EQ(rule.netSpeed, unsigned{0});
+}
