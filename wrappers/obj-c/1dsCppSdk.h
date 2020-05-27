19f3e5f2744eec6bf62354d83f5b239020f40a27
David Brown <dabrow@microsoft.com>
2020-05-27 10:32:20 -0700

    Add umbrella header for obj-c wrappers
    Add imports for all public obj-c headers to support Swift

---
+++ wrappers/obj-c/1dsCppSdk.h
@@ -0,0 +1,11 @@
+#ifndef 1dsCppSdk_h
+#define 1dsCppSdk_h
+
+#import "ODWDiagnosticDataViewer.h"
+#import "ODWEventProperties.h"
+#import "ODWLogConfiguration.h"
+#import "ODWLogger.h"
+#import "ODWLogManager.h"
+#import "ODWSemanticContext.h"
+
+#endif
