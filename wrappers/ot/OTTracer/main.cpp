6e4d4ef8476d8cf8e57ee33d365bf9af9531372a
Max Golovanov <maxgolov@microsoft.com>
2020-06-05 11:27:03 -0700

    Switch to Provider GUID of IFXSDKCI and avoid dots in event names

--- wrappers/ot/OTTracer/main.cpp
+++ wrappers/ot/OTTracer/main.cpp
@@ -33,23 +33,24 @@ using M = std::map<std::string_view, std::string_view>;
 void test_spans()
 {
     oneds::TracerProvider tp;
-    auto tracer = tp.GetTracer("6d084bbf6a9644ef83f40a77c9e34580", CONFIG_ETW);
+    // auto tracer = tp.GetTracer("6d084bbf6a9644ef83f40a77c9e34580", CONFIG_ETW);
+    auto tracer = tp.GetTracer("8f667dcec4aa5f17a06bcadb2ca760f3", CONFIG_ETW);
     auto span   = tracer->StartSpan("MySpan");
 
     // add m1 to span 1
     M m1 = {{"key1", "one"}, {"key2", "two"}};
-    span->AddEvent("MyProduct.MyEvent1", m1);
+    span->AddEvent("MyProductMyEvent1", m1);
 
     // add m2 to span 2
     M m2 = {{"key1", "one"}, {"key2", "two"}};
-    span->AddEvent("MyProduct.MyEvent2", m2);
+    span->AddEvent("MyProductMyEvent2", m2);
 
     // add map to span using initializer_list
-    span->AddEvent("MyProduct.MyEvent3", {{"key1", "one"}, {"key2", "two"}});
+    span->AddEvent("MyProductMyEvent3", {{"key1", "one"}, {"key2", "two"}});
 
     // Transform from EventProperties to collection of variant (AttributeValue)
     event::Properties myEvent(
-        "MyProduct.MyEvent4",
+        "MyProductMyEvent4",
         {/* C-string */ {"key1", "value1"},
          /* int32_t  */ {"intKey", 12345},
          /* bool     */ {"boolKey", static_cast<bool>(true)},
@@ -58,6 +59,11 @@ void test_spans()
     auto name = myEvent.GetName();
     span->AddEvent(nostd::string_view(name.c_str(), name.length()), myEvent);
 
+    // add map to span using initializer_list
+    span->AddEvent("TestEvent1", {{"TestEvent1", "one"}}); // maps to T1
+
+    // add map to span using initializer_list
+    span->AddEvent("TestEvent2", {{"TestEvent2", "two"}}); // maps to T2
     span->End();
     // end tracing session
     tracer->Close();
