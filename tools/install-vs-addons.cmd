b620203f6634ce0fefb1343f326f90c130614774
Max Golovanov <maxgolov@microsoft.com>
2020-06-05 23:15:34 -0700

    Update install-vs-addons.cmd
    It's not that easy to troubleshoot this script on GitHub action

--- tools/install-vs-addons.cmd
+++ tools/install-vs-addons.cmd
@@ -6,33 +6,33 @@ REM Download Visual Studio LLVM extension required for clang build to succeed
 call download.cmd https://llvmextensions.gallerycdn.vsassets.io/extensions/llvmextensions/llvm-toolchain/1.0.363769/1560930595399/llvm.vsix
 
 REM Install optional components required for ARM build - vs2017-BuildTools
-IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools" (
-"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vs_installer.exe" ^
-	-- modify --installPath "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools" -q ^
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools" (
+  start /wait "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vs_installer.exe" ^
+	modify --installPath "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools" -q ^
 	--add Microsoft.VisualStudio.Component.VC.ATL ^
 	--add Microsoft.VisualStudio.Component.VC.ATL.ARM ^
-	--add Microsoft.VisualStudio.Component.VC.ATL.ARM64
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\Common7\IDE\VSIXInstaller.exe" /q /a llvm.vsix
+	--add Microsoft.VisualStudio.Component.VC.ATL.ARM64 --passive --wait
+  "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\Common7\IDE\VSIXInstaller.exe" /q /a llvm.vsix
 )
 
 REM Install optional components required for ARM build - vs2017-Enterprise
-IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise" (
-"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vs_installer.exe" ^
-	-- modify --installPath "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise" -q ^
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise" (
+  start /wait "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vs_installer.exe" ^
+	modify --installPath "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise" -q ^
 	--add Microsoft.VisualStudio.Component.VC.ATL ^
 	--add Microsoft.VisualStudio.Component.VC.ATL.ARM ^
-	--add Microsoft.VisualStudio.Component.VC.ATL.ARM64
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\VSIXInstaller.exe" /q /a llvm.vsix
+	--add Microsoft.VisualStudio.Component.VC.ATL.ARM64 --passive --wait
+  "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\VSIXInstaller.exe" /q /a llvm.vsix
 )
 
 REM Install optional components required for ARM build - vs2019-Enterprise
-IF EXIST %ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise (
-"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe" ^
-	-- modify --installPath "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise" -q ^
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise" (
+  start /wait "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe" ^
+	modify --installPath "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise" -q ^
 	--add Microsoft.VisualStudio.Component.VC.ATL ^
 	--add Microsoft.VisualStudio.Component.VC.ATL.ARM ^
-	--add Microsoft.VisualStudio.Component.VC.ATL.ARM64
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\VSIXInstaller.exe" /q /a llvm.vsix
+	--add Microsoft.VisualStudio.Component.VC.ATL.ARM64 --passive --wait
+  "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\VSIXInstaller.exe" /q /a llvm.vsix
 )
 
 REM Ignore failures if components have been already installed
