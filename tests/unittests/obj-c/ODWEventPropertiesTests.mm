51617d37e62f123e122ac5459b284d5c27e5d780
David Brown <dabrow@microsoft.com>
2020-07-29 15:22:05 -0700

    Add ODWLogConfiguration tests

--- tests/unittests/obj-c/ODWEventPropertiesTests.mm
+++ tests/unittests/obj-c/ODWEventPropertiesTests.mm
@@ -15,14 +15,6 @@
 
 @implementation ODWLogManagerTests
 
-- (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-}
-
 - (void)testInitWithName {
     ODWEventProperties* eventProps = [[ODWEventProperties alloc] initWithName:@"MyEvent"];
     XCTAssertEqual(eventProps.name, @"MyEvent");
