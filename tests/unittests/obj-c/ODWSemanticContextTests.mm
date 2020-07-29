5dac630393effdffc7bed7ce26782eb20af23136
David Brown <dabrow@microsoft.com>
2020-07-29 15:22:05 -0700

    Add ODWLogConfiguration tests

--- tests/unittests/obj-c/ODWSemanticContextTests.mm
+++ tests/unittests/obj-c/ODWSemanticContextTests.mm
@@ -33,14 +33,6 @@ public:
 
 @implementation ODWSemanticContextTests
 
-- (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-}
-
 - (void)testSetAppId {
     TestSemanticContext nativeContext;
     ODWSemanticContext* context = [[ODWSemanticContext alloc] initWithISemanticContext:&nativeContext];
