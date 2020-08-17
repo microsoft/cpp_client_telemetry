6f9e6cbee26b1bb568052e9f48d0e434a000a632
Lalit Kumar Bhasin <labhas@microsoft.com>
2020-08-17 10:19:22 +0000

    more changes

--- lib/offline/OfflineStorageFactory.cpp
+++ lib/offline/OfflineStorageFactory.cpp
@@ -1,33 +1,31 @@
 // Copyright (c) Microsoft. All rights reserved.
 #include "mat/config.h"
 #include "OfflineStorageFactory.hpp"
-#include "pal/PAL.hpp"
 
 namespace ARIASDK_NS_BEGIN
 {
 #ifdef HAVE_MAT_STORAGE
 #ifdef USE_OFFLINE_STORAGE_MODULE
-IOfflineStorage* OfflineStorageFactory::Create(ILogManager& logManager, IRuntimeConfig& runtimeConfig)
+std::shared_ptr<IOfflineStorage> OfflineStorageFactory::Create(ILogManager& logManager, IRuntimeConfig& runtimeConfig)
 {
     LOG_TRACE("Creating OfflineStorage from module");
-    auto ptr = std::static_pointer_cast<IOfflineStorage>(logManager.GetLogConfiguration().GetModule(CFG_MODULE_OFFLINE_STORAGE));
-    return ptr.get();
+    return std::static_pointer_cast<IOfflineStorage>(logManager.GetLogConfiguration().GetModule(CFG_MODULE_OFFLINE_STORAGE));
 }
 #elif USE_ROOM
-IOfflineStorage* OfflineStorageFactory::Create(ILogManager& logManager, IRuntimeConfig& runtimeConfig)
+std::shared_ptr<IOfflineStorage> OfflineStorageFactory::Create(ILogManager& logManager, IRuntimeConfig& runtimeConfig)
 {
     LOG_TRACE("Creating OfflineStorage_Room");
-    return new OfflineStorage_Room(logManager, runtimeConfig);
+    return std:shared_ptr<IOfflineStorage>(new OfflineStorage_Room(logManager, runtimeConfig));
 }
 #else
-IOfflineStorage* OfflineStorageFactory::Create(ILogManager& logManager, IRuntimeConfig& runtimeConfig)
+std::shared_ptr<IOfflineStorage> OfflineStorageFactory::Create(ILogManager& logManager, IRuntimeConfig& runtimeConfig)
 {
-    LOG_TRACE("Creating HttpClient_WinRt");
-    return new OfflineStorage_SQLite(logManager, runtimeConfig);
+    LOG_TRACE("Creating OfflineStorage_SQLite");
+    return std::shared_ptr<IOfflineStorage>(new OfflineStorage_SQLite(logManager, runtimeConfig));
 }
 #endif // USE_OFFLINE_STORAGE_MODULE
 #else
-IOfflineStorage* OfflineStorageFactory::Create(ILogManager& logManager, IRuntimeConfig& runtimeConfig)
+std::shared_ptr<IOfflineStorage> OfflineStorageFactory::Create(ILogManager& logManager, IRuntimeConfig& runtimeConfig)
 {
     LOG_TRACE("MAT storage disabled");
     return nullptr;
