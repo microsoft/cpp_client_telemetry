cb0730b143a09be9cc16a96b455c0d1dfa5b48ea
Benoit Martin <bemartin@microsoft.com>
2020-09-03 16:11:39 -0700

    Created tests projects

--- wrappers/xamarin/sdk/OneDsCppSdk.Standard/Logger.cs
+++ wrappers/xamarin/sdk/OneDsCppSdk.Standard/Logger.cs
@@ -2,22 +2,7 @@
 {
     public class Logger : ILogger
     {
-        public ISemanticContext GetSemanticContext()
-        {
-            return default(ISemanticContext);
-        }
-
-        public void LogAggregatedMetric(string name, long duration, long count, EventProperties properties)
-        {
-        }
-
-        public void LogAggregatedMetric(AggregatedMetricData metricData, EventProperties properties)
-        {
-        }
-
-        public void LogAppLifecycle(AppLifecycleState state, EventProperties properties)
-        {
-        }
+        public ISemanticContext SemanticContext => default;
 
         public void LogEvent(string name)
         {
@@ -35,14 +20,6 @@
         {
         }
 
-        public void LogPageAction(string pageViewId, ActionType actionType, EventProperties properties)
-        {
-        }
-
-        public void LogPageAction(PageActionData pageActionData, EventProperties properties)
-        {
-        }
-
         public void LogPageView(string id, string pageName, EventProperties properties)
         {
         }
@@ -51,67 +28,11 @@
         {
         }
 
-        public void LogSampledMetric(string name, double value, string units, EventProperties properties)
-        {
-        }
-
-        public void LogSampledMetric(string name, double value, string units, string instanceName, string objectClass, string objectId, EventProperties properties)
-        {
-        }
-
         public void LogSession(SessionState state, EventProperties properties)
         {
         }
 
         public void LogTrace(TraceLevel level, string message, EventProperties properties)
-        {
-        }
-
-        public void LogUserState(UserState state, long timeToLiveInMillis, EventProperties properties)
-        {
-        }
-
-        public void SetContext(string name, string value, PiiKind piiKind = PiiKind.None)
-        {
-        }
-
-        public void SetContext(string name, double value, PiiKind piiKind = PiiKind.None)
-        {
-        }
-
-        public void SetContext(string name, long value, PiiKind piiKind = PiiKind.None)
-        {
-        }
-
-        public void SetContext(string name, sbyte value, PiiKind piiKind = PiiKind.None)
-        {
-        }
-
-        public void SetContext(string name, short value, PiiKind piiKind = PiiKind.None)
-        {
-        }
-
-        public void SetContext(string name, int value, PiiKind piiKind = PiiKind.None)
-        {
-        }
-
-        public void SetContext(string name, byte value, PiiKind piiKind = PiiKind.None)
-        {
-        }
-
-        public void SetContext(string name, ushort value, PiiKind piiKind = PiiKind.None)
-        {
-        }
-
-        public void SetContext(string name, uint value, PiiKind piiKind = PiiKind.None)
-        {
-        }
-
-        public void SetContext(string name, ulong value, PiiKind piiKind = PiiKind.None)
-        {
-        }
-
-        public void SetContext(string name, bool value, PiiKind piiKind = PiiKind.None)
         {
         }
     }
