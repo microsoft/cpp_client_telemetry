393fe25202cbe06576ad7f4a4cc7ea33cd7db895
Benoit Martin <bemartin@microsoft.com>
2020-09-08 15:14:49 -0700

    Update to build-xamarin.sh script 

--- build-xamarin.sh
+++ build-xamarin.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
-if [ "$1" == "skip_build" ]; then
+if [ "$1" == "skipBuild" ] || [ "$2" == "skipBuild" ]; then
 SKIP_BUILD=true
 else
 SKIP_BUILD=false
 fi
 
-if [ ! SKIP_BUILD ]; then
+if [ "$1" == "cleanXamarin" ] || [ "$1" == "cleanAll" ]; then
+rm -rf ./wrappers/xamarin/sdk/OneDsCppSdk.iOS.Bindings/obj
+rm -rf ./wrappers/xamarin/sdk/OneDsCppSdk.iOS.Bindings/bin
+rm "./wrappers/xamarin/sdk/OneDsCppSdk.iOS.Bindings/Native References/libmat.a"
+
+rm -rf ./wrappers/xamarin/sdk/OneDsCppSdk.Android.Bindings/obj
+rm -rf ./wrappers/xamarin/sdk/OneDsCppSdk.Android.Bindings/bin
+rm -rf ./wrappers/xamarin/sdk/OneDsCppSdk.Android.Bindings/lib
+rm ./wrappers/xamarin/sdk/OneDsCppSdk.Android.Bindings/Jars/maesdk-release.aar
+
+rm -rf ./wrappers/xamarin/sdk/OneDsCppSdk.Standard/obj
+rm -rf ./wrappers/xamarin/sdk/OneDsCppSdk.Standard/bin
+fi
+
+if [ "$SKIP_BUILD" == "false" ]; then
 
 # Build for iOS
 ./build-ios.sh release arm64
@@ -30,3 +44,7 @@ rsync -a ./lib/android_build/maesdk/build/intermediates/cmake/release/obj/x86_64
 rsync -a ./lib/android_build/maesdk/build/outputs/aar/maesdk-release.aar ./wrappers/xamarin/sdk/OneDsCppSdk.Android.Bindings/Jars/
 
 # Build Xamarin Bindings Solution
+pushd ./wrappers/xamarin
+/Library/Frameworks/Mono.framework/Versions/6.12.0/bin/msbuild ./Microsoft.Applications.Events.Xamarin.sln /p:Configuration=Release -restore -v:minimal
+nuget pack ./Microsoft.Applications.Events.nuspec
+popd
