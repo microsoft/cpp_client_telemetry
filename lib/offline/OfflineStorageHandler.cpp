577d18415da4818f97793b2ce2f08ae7febf30f3
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2020-09-22 19:06:44 +0530

    merge conflict

--- lib/offline/OfflineStorageHandler.cpp
+++ lib/offline/OfflineStorageHandler.cpp
@@ -1,14 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 
 #include "OfflineStorageHandler.hpp"
-
-#ifdef HAVE_MAT_STORAGE
-#ifdef USE_ROOM
-#include "offline/OfflineStorage_Room.hpp"
-#else
-#include "offline/OfflineStorage_SQLite.hpp"
-#endif
-#endif
+#include "OfflineStorageFactory.hpp"
 
 #include "offline/MemoryStorage.hpp"
 
@@ -22,8 +15,8 @@ namespace MAT_NS_BEGIN {
 
     MATSDK_LOG_INST_COMPONENT_CLASS(OfflineStorageHandler, "EventsSDK.StorageHandler", "Events telemetry client - OfflineStorageHandler class");
 
-    OfflineStorageHandler::OfflineStorageHandler(ILogManager& logManager, IRuntimeConfig& runtimeConfig, ITaskDispatcher& taskDispatcher)
-        : m_logManager(logManager),
+    OfflineStorageHandler::OfflineStorageHandler(ILogManager& logManager, IRuntimeConfig& runtimeConfig, ITaskDispatcher& taskDispatcher) :
+        m_logManager(logManager),
         m_config(runtimeConfig),
         m_taskDispatcher(taskDispatcher),
         m_killSwitchManager(),
@@ -38,7 +31,7 @@ namespace MAT_NS_BEGIN {
         m_queryDbSize(0),
         m_isStorageFullNotificationSend(false)
     {
-        // FIXME: [MG] - this code seems redundant / suspicious because OfflineStorage_SQLite.cpp is doing the same thing...
+        // TODO: [MG] - OfflineStorage_SQLite.cpp is performing similar checks
         uint32_t percentage = m_config[CFG_INT_RAMCACHE_FULL_PCT];
         uint32_t cacheMemorySizeLimitInBytes = m_config[CFG_INT_RAM_QUEUE_SIZE];
         if (percentage > 0 && percentage <= 100)
@@ -46,7 +39,8 @@ namespace MAT_NS_BEGIN {
             m_memoryDbSizeNotificationLimit = (percentage * cacheMemorySizeLimitInBytes) / 100;
         }
         else
-        {// incase user has specified bad percentage, we stck to 75%
+        {
+            // In case if user has specified bad percentage, we stick to 75%
             m_memoryDbSizeNotificationLimit = (DB_FULL_NOTIFICATION_DEFAULT_PERCENTAGE * cacheMemorySizeLimitInBytes) / 100;
         }
     }
@@ -87,17 +81,8 @@ namespace MAT_NS_BEGIN {
         m_observer = &observer;
         uint32_t cacheMemorySizeLimitInBytes = m_config[CFG_INT_RAM_QUEUE_SIZE];
 
-#ifndef HAVE_MAT_STORAGE
-        /* No storage configured */
-        m_offlineStorageDisk.reset(nullptr);
-#else
-#ifdef USE_ROOM
-        m_offlineStorageDisk.reset(new OfflineStorage_Room(m_logManager, m_config));
-#else
-        m_offlineStorageDisk.reset(new OfflineStorage_SQLite(m_logManager, m_config));
-#endif
+        m_offlineStorageDisk = OfflineStorageFactory::Create(m_logManager, m_config);
         m_offlineStorageDisk->Initialize(*this);
-#endif
 
         // TODO: [MG] - consider passing m_offlineStorageDisk to m_offlineStorageMemory,
         // so that the Flush() op on memory storage leads to saving unflushed events to
@@ -228,18 +213,6 @@ namespace MAT_NS_BEGIN {
         {
             auto memDbSize = m_offlineStorageMemory->GetSize();
             {
-#if 0
-                //check if Application needs to be notified
-                if ((memDbSize > m_memoryDbSizeNotificationLimit) && !m_isStorageFullNotificationSend)
-                {
-                    // TODO: [MG] - do we really need in-memory DB size limit notifications here?
-                    DebugEvent evt;
-                    evt.type = DebugEventType::EVT_STORAGE_FULL;
-                    evt.param1 = 1;
-                    m_logManager.DispatchEvent(evt);
-                    m_isStorageFullNotificationSend = true;
-                }
-#endif
                 // During flush, this will block on a mutex while records
                 // are selected and removed from the cache (but will
                 // not block for the subsequent handoff to persistent
@@ -277,10 +250,13 @@ namespace MAT_NS_BEGIN {
         return true;
     }
 
-    size_t OfflineStorageHandler::StoreRecords(std::vector<StorageRecord> & records) {
+    size_t OfflineStorageHandler::StoreRecords(std::vector<StorageRecord>& records)
+    {
         size_t stored = 0;
-        for (auto & i : records) {
-            if (StoreRecord(i)) {
+        for (auto& i : records)
+        {
+            if (StoreRecord(i))
+            {
                 ++stored;
             }
         }
@@ -344,10 +320,9 @@ namespace MAT_NS_BEGIN {
             }
         }
 
-        if (m_config.IsClockSkewEnabled()
-            && !m_clockSkewManager.GetResumeTransmissionAfterClockSkew()
+        if (m_config.IsClockSkewEnabled() && !m_clockSkewManager.GetResumeTransmissionAfterClockSkew()
             /* && !consumedIds.empty() */
-            )
+        )
         {
             m_clockSkewManager.GetDelta();
         }
@@ -372,12 +347,12 @@ namespace MAT_NS_BEGIN {
      * invoked by HTTP callback thread. The scrubbing is done
      * async in context where the HTTP callback is running.
      */
-    void OfflineStorageHandler::DeleteRecordsByKeys(const std::list<std::string> & keys)
+    void OfflineStorageHandler::DeleteRecordsByKeys(const std::list<std::string>& keys)
     {
-        for (const auto & key : keys)
+        for (const auto& key : keys)
         {
             /* DELETE * FROM events WHERE tenant_token=${key} */
-            DeleteRecords({ { "tenant_token", key } });
+            DeleteRecords({{"tenant_token", key}});
         }
     }
 
@@ -400,17 +375,17 @@ namespace MAT_NS_BEGIN {
     /**
      * Perform scrub of both memory queue and offline storage.
      */
-     /// <summary>
-     /// Perform scrub of underlying storage systems using 'where' clause
-     /// </summary>
-     /// <param name="whereFilter">The where filter.</param>
-     /// <remarks>
-     /// whereFilter contains the key-value pairs for the
-     /// WHERE [key0==value0 .. keyN==valueN] clause.
-     /// </remarks>
-    void OfflineStorageHandler::DeleteRecords(const std::map<std::string, std::string> & whereFilter)
+    /// <summary>
+    /// Perform scrub of underlying storage systems using 'where' clause
+    /// </summary>
+    /// <param name="whereFilter">The where filter.</param>
+    /// <remarks>
+    /// whereFilter contains the key-value pairs for the
+    /// WHERE [key0==value0 .. keyN==valueN] clause.
+    /// </remarks>
+    void OfflineStorageHandler::DeleteRecords(const std::map<std::string, std::string>& whereFilter)
     {
-        for (const auto storagePtr : { m_offlineStorageMemory.get() , m_offlineStorageDisk.get() })
+        for (const auto storagePtr : {m_offlineStorageMemory.get(), m_offlineStorageDisk.get()})
         {
             if (storagePtr != nullptr)
             {
@@ -446,7 +421,7 @@ namespace MAT_NS_BEGIN {
         }
 
         LOG_TRACE(" OfflineStorageHandler Deleting %u sent event(s) {%s%s}...",
-            static_cast<unsigned>(ids.size()), ids.front().c_str(), (ids.size() > 1) ? ", ..." : "");
+                  static_cast<unsigned>(ids.size()), ids.front().c_str(), (ids.size() > 1) ? ", ..." : "");
         if (fromMemory && nullptr != m_offlineStorageMemory)
         {
             m_offlineStorageMemory->DeleteRecords(ids, headers, fromMemory);
