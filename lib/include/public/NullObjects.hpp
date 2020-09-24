bb62c2f40f8cc42e19143da5ac4bbf3f59c45d42
Lalit Bhasin <labhas@microsoft.com>
2020-09-24 17:53:09 +0530

    fix indent

--- lib/include/public/NullObjects.hpp
+++ lib/include/public/NullObjects.hpp
@@ -376,10 +376,10 @@ namespace MAT_NS_BEGIN
             return nullDataViewerCollection;
         }
 
-		virtual status_t DeleteData() noexcept override 
-		{ 
-			return STATUS_ENOSYS;
-		}
+        virtual status_t DeleteData() noexcept override 
+        { 
+            return STATUS_ENOSYS;
+        }
 
         private:
             NullDataViewerCollection nullDataViewerCollection;
