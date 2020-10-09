22756e0bd5d9ed978727bee537a18be55f21c8bd
Max Golovanov <maxgolov@microsoft.com>
2020-10-09 01:30:32 -0700

    Addressing code review comments

--- lib/android_build/ide.cmd
+++ lib/android_build/ide.cmd
@@ -6,13 +6,12 @@ for /F "usebackq skip=2 tokens=1-2*" %%A in (`reg.exe QUERY "HKLM\Software\Andro
 
 echo Android Studio Path: %AndroidStudioPath%
 set "PATH=%AndroidStudioPath%\bin;%PATH%"
-if "%ANDROID_SDK_ROOT%"== "" (
-  echo Set default Android SDK path ...
-  set "ANDROID_SDK_ROOT=C:\Android\android-sdk"
-  set "ANDROID_HOME=%ANDROID_SDK_ROOT%"
-  set "ANDROID_NDK=%ANDROID_SDK_ROOT%\ndk\21.1.6352462"
-  set "ANDROID_NDK_HOME=%ANDROID_NDK%"
-)
+
+if "%ANDROID_SDK_ROOT%"   == "" set "ANDROID_SDK_ROOT=C:\Android\android-sdk"
+if "%ANDROID_HOME%"       == "" set "ANDROID_HOME=%ANDROID_SDK_ROOT%"
+if "%ANDROID_NDK_VERSION%"== "" set "ANDROID_NDK_VERSION=21.1.6352462"
+if "%ANDROID_NDK%"        == "" set "ANDROID_NDK=%ANDROID_SDK_ROOT%\ndk\%ANDROID_NDK_VERSION%"
+if "%ANDROID_NDK_HOME%"   == "" set "ANDROID_NDK_HOME=%ANDROID_NDK%"
 
 where studio.bat >NUL 2>NUL
 if %ERRORLEVEL% neq 0 (
