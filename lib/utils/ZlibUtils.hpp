a958fab5f45892ae3e2f3fe9d4d26b67cfa274a7
Alexandr Gavrishev <algavris@microsoft.com>
2020-10-25 12:33:13 +0200

    Extract InflateVector to ZlibUtils for reuse to fix crash in iOS

---
+++ lib/utils/ZlibUtils.hpp
@@ -0,0 +1,21 @@
+//
+// Copyright (c) 2015-2020 Microsoft Corporation and Contributors.
+// SPDX-License-Identifier: Apache-2.0
+//
+#ifndef LIB_ZLIB_UTILS_HPP
+#define LIB_ZLIB_UTILS_HPP
+
+#include "Version.hpp"
+#include <vector>
+
+namespace MAT_NS_BEGIN 
+{
+    class ZlibUtils
+    {
+        public:
+            static bool InflateVector(const std::vector<uint8_t>& in, std::vector<uint8_t>& out, bool isGzip);
+    };
+
+} MAT_NS_END
+
+#endif
