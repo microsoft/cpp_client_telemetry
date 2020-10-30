446e096b48092a5245ce1e56ed047ac9acc6dad1
vong srey <vosrey@microsoft.com>
2020-10-30 15:46:16 -0700

    Add Common Diagnostic Stack Factory interface and hook to implementation in CDS submodule

---
+++ lib/include/public/ICdsFactory.hpp
@@ -0,0 +1,54 @@
+//
+// Copyright (c) 2015-2020 Microsoft Corporation and Contributors.
+// SPDX-License-Identifier: Apache-2.0
+//
+#ifndef MAT_ICDSFACTORY_HPP
+#define MAT_ICDSFACTORY_HPP
+
+#include "ctmacros.hpp"
+
+#include <functional>
+#include <map>
+#include <string>
+
+namespace Microsoft {
+namespace CDS {
+
+struct Config {
+    std::wstring guid;
+    std::map<std::wstring /*param name*/, std::wstring /*param value*/> params;
+};
+
+struct DiagnosticConfig {
+    Config collectorConfig;
+    Config pakagerConfig;
+    Config uploaderConfig;
+};
+
+using CollectorCallback = std::function<void(const std::wstring&, const Config&)>;
+
+
+/// <summary>
+/// 
+/// </summary>
+class MATSDK_LIBABI ICommonDiagnosticStack {
+public:
+    /// <summary>
+    /// 
+    /// </summary>
+    virtual void RegisterCollector(std::wstring collectorGuid, CollectorCallback callback) noexcept = 0;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    virtual void DoCollectAndUpload(std::wstring sessionID, DiagnosticConfig config) noexcept = 0;
+};
+
+
+} // namespace CDS
+} // namespace Microsoft
+
+MATSDK_LIBABI const std::shared_ptr<Microsoft::CDS::ICommonDiagnosticStack>& GetCommonDiagnosticStack();
+
+#endif
+
