3dcb229ae2934151bd574107eeff7aa9fb1a5f8e
Hung Ho <hohung@microsoft.com>
2021-02-19 10:42:20 -0800

    config

--- wrappers/obj-c/ODWLogConfiguration.h
+++ wrappers/obj-c/ODWLogConfiguration.h
@@ -312,114 +312,114 @@ extern NSString * _Nonnull const ODWCFG_BOOL_SESSION_RESET_ENABLED;
 @interface ODWLogConfiguration : NSObject
 
 /*!
- @brief Return a copy of the default configuration
+@brief Return a copy of the default configuration
 */
 +(nullable ODWLogConfiguration *)getLogConfigurationCopy;
 
 /*!
- @brief Sets the URI of the event collector.
- @param eventCollectorUri A string for event collector uri.
+@brief Sets the URI of the event collector.
+@param eventCollectorUri A string for event collector uri.
 */
 +(void)setEventCollectorUri:(nonnull NSString *)eventCollectorUri;
 
 /*!
- @brief Returns the URI of the event collector.
+@brief Returns the URI of the event collector.
 */
 +(nullable NSString *)eventCollectorUri;
 
 /*!
- @brief Sets the RAM queue size limit in bytes.
- @param cacheMemorySizeLimitInBytes  A long value for memory size limit in bytes.
+@brief Sets the RAM queue size limit in bytes.
+@param cacheMemorySizeLimitInBytes  A long value for memory size limit in bytes.
 */
 +(void)setCacheMemorySizeLimitInBytes:(uint64_t)cacheMemorySizeLimitInBytes;
 
 /*!
- @brief return the RAM queue size limit in bytes.
+@brief return the RAM queue size limit in bytes.
 */
 +(uint64_t)cacheMemorySizeLimitInBytes;
 
 /*!
- @brief Sets the size limit of the disk file used to cache events on the client side.
- @param cacheFileSizeLimitInBytes  A long value for cache file size limit.
+@brief Sets the size limit of the disk file used to cache events on the client side.
+@param cacheFileSizeLimitInBytes  A long value for cache file size limit.
 */
 +(void)setCacheFileSizeLimitInBytes:(uint64_t)cacheFileSizeLimitInBytes;
 
 /*!
- @brief Returns the size limit of the disk file used to cache events on the client side.
+@brief Returns the size limit of the disk file used to cache events on the client side.
 */
 +(uint64_t)cacheFileSizeLimitInBytes;
 
 /*!
- @brief Sets max teardown upload time in seconds.
- @param maxTeardownUploadTimeInSec An integer that time in seconds.
+@brief Sets max teardown upload time in seconds.
+@param maxTeardownUploadTimeInSec An integer that time in seconds.
 */
 +(void)setMaxTeardownUploadTimeInSec:(int)maxTeardownUploadTimeInSec;
 
 /*!
- @brief Sets if trace logging to file is enabled.
- @param enableTrace True if tracing is enabled.
+@brief Sets if trace logging to file is enabled.
+@param enableTrace True if tracing is enabled.
 */
 +(void)setEnableTrace:(bool)enableTrace;
 
 /*!
- @brief Sets if console logging from the iOS wrapper is enabled.
- @param enableConsoleLogging True if logging is enabled.
+@brief Sets if console logging from the iOS wrapper is enabled.
+@param enableConsoleLogging True if logging is enabled.
 */
 +(void)setEnableConsoleLogging:(bool)enableConsoleLogging;
 
 /*!
- @brief Sets the internal SDK debugging trace level.
- @param TraceLevel one of the ACTTraceLevel values.
+@brief Sets the internal SDK debugging trace level.
+@param TraceLevel one of the ACTTraceLevel values.
 */
 +(void)setTraceLevel:(int)TraceLevel;
 
 /*!
- @brief Returns true if tracing is enabled.
+@brief Returns true if tracing is enabled.
 */
 +(bool)enableTrace;
 
 /*!
- @brief Returns true if console logging is enabled.
+@brief Returns true if console logging is enabled.
 */
 +(bool)enableConsoleLogging;
 
 /*!
- @brief Sets if inner C++ exceptions should be surfaced to Wrapper consumers.
- @param surfaceCppExceptions True if C++ exceptions should be surfaced.
+@brief Sets if inner C++ exceptions should be surfaced to Wrapper consumers.
+@param surfaceCppExceptions True if C++ exceptions should be surfaced.
 */
 +(void)setSurfaceCppExceptions:(bool)surfaceCppExceptions;
 
 /*!
- @brief Returns true if inner C++ exceptions are surfaced to Wrapper consumers.
+@brief Returns true if inner C++ exceptions are surfaced to Wrapper consumers.
 */
 +(bool)surfaceCppExceptions;
 
 /*!
- @brief Sets if session timestamp should be reset after a session ends
- @param enableSessionReset True if session should be reset on session end.
+@brief Sets if session timestamp should be reset after a session ends
+@param enableSessionReset True if session should be reset on session end.
 */
 +(void)setEnableSessionReset:(bool)enableSessionReset;
 
 /*!
- @brief Returns true if session will be reset on session end.
+@brief Returns true if session will be reset on session end.
 */
 +(bool)enableSessionReset;
 
 /*!
- @brief Sets the cache file path.
- @param cacheFilePath A string for the cache path.
+@brief Sets the cache file path.
+@param cacheFilePath A string for the cache path.
 */
 +(void)setCacheFilePath:(nonnull NSString *)cacheFilePath;
 
 /*!
- @brief Returns the cache file path.
+@brief Returns the cache file path.
 */
 +(nullable NSString *)cacheFilePath;
 
 /*!
- @brief Sets a config key to a string value
- @param key A key.
- @param value A value.
+@brief Sets a config key to a string value for the copied config
+@param key A key.
+@param value A value.
 */
 -(void)set:(nonnull NSString *)key withValue:(nonnull NSString *)value;
 
