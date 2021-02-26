669042caf33f513228cde306c65e14dfce8d14bc
Ronnie P <rpathak3@buffalo.edu>
2021-02-26 14:07:10 -0800

    expose file cache size limit and db drop property in C# projection (#794)

--- lib/shared/LogConfigurationCX.cpp
+++ lib/shared/LogConfigurationCX.cpp
@@ -17,13 +17,15 @@ namespace MATW_NS_BEGIN {
     {
         MAT::ILogConfiguration& configurationCore = MAT::LogManager::GetLogConfiguration();
         configurationCore[MAT::CFG_INT_SDK_MODE] = (int)this->SdkMode;
-        configurationCore[MAT::CFG_STR_CACHE_FILE_PATH]    = FromPlatformString(this->OfflineStorage);
-        configurationCore[MAT::CFG_STR_COLLECTOR_URL]      = FromPlatformString(this->CollectorURL);
-        configurationCore[MAT::CFG_INT_TRACE_LEVEL_MASK]   = this->TraceLevelMask;
-        configurationCore[MAT::CFG_INT_TRACE_LEVEL_MIN]    = (int)(this->MinTraceLevel);
-        configurationCore[MAT::CFG_INT_MAX_TEARDOWN_TIME]  = this->MaxTeardownUploadTimeInSec;
-        configurationCore[MAT::CFG_INT_MAX_PENDING_REQ]    = this->MaxPendingHTTPRequests;
-        configurationCore[MAT::CFG_INT_RAM_QUEUE_BUFFERS]  = this->MaxDBFlushQueues;
+        configurationCore[MAT::CFG_STR_CACHE_FILE_PATH]         = FromPlatformString(this->OfflineStorage);
+        configurationCore[MAT::CFG_INT_CACHE_FILE_SIZE]         = this->CacheFileSizeLimitInBytes;
+        configurationCore[MAT::CFG_BOOL_ENABLE_DB_DROP_IF_FULL] = this->EnableDBDropIfFull;
+        configurationCore[MAT::CFG_STR_COLLECTOR_URL]           = FromPlatformString(this->CollectorURL);
+        configurationCore[MAT::CFG_INT_TRACE_LEVEL_MASK]        = this->TraceLevelMask;
+        configurationCore[MAT::CFG_INT_TRACE_LEVEL_MIN]         = (int)(this->MinTraceLevel);
+        configurationCore[MAT::CFG_INT_MAX_TEARDOWN_TIME]       = this->MaxTeardownUploadTimeInSec;
+        configurationCore[MAT::CFG_INT_MAX_PENDING_REQ]         = this->MaxPendingHTTPRequests;
+        configurationCore[MAT::CFG_INT_RAM_QUEUE_BUFFERS]       = this->MaxDBFlushQueues;
 
         // Populate these two fields at C++ core only if not empty at C# projection
         if (!this->StartProfileName->Equals(""))
