cd4584e7a34096fc9d0b470ee0fb94967848e372
Matt Koscumb <makoscum@microsoft.com>
2021-04-14 12:51:24 -0700

    This warning supression wasn't actually doing anything, so I've removed it.

--- lib/api/LogManagerImpl.cpp
+++ lib/api/LogManagerImpl.cpp
@@ -2,10 +2,6 @@
 // Copyright (c) 2015-2020 Microsoft Corporation and Contributors.
 // SPDX-License-Identifier: Apache-2.0
 //
-#ifdef _MSC_VER
-// evntprov.h(838) : warning C4459 : declaration of 'Version' hides global declaration
-#pragma warning(disable : 4459)
-#endif
 #include "LogManagerImpl.hpp"
 #include "mat/config.h"
 
