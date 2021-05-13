e064eb742632006b6b98cd5ea2e47cd9defeaeaa
jevery23 <joevery@microsoft.com>
2021-05-12 18:29:52 -0700

    Move include to first in order.

--- lib/decoder/PayloadDecoder.cpp
+++ lib/decoder/PayloadDecoder.cpp
@@ -2,9 +2,9 @@
 // Copyright (c) 2015-2020 Microsoft Corporation and Contributors.
 // SPDX-License-Identifier: Apache-2.0
 //
-#include "PayloadDecoder.hpp"
-
 #include "mat/config.h"
+
+#include "PayloadDecoder.hpp"
 
 #if !defined(HAVE_MAT_ZLIB) || !defined(HAVE_MAT_JSONHPP)
 
