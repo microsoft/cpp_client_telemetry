715f6e2dba9d5c1b8fa3e0a0566af2c4bbf86dcf
Max Golovanov <maxgolov@microsoft.com>
2021-05-27 16:41:16 -0700

    Move to PlatformToolset variable (#882)

--- tools/build-deps.cmd
+++ tools/build-deps.cmd
@@ -24,10 +24,10 @@ msbuild %SOLUTION% /target:sqlite,zlib,sqlite-uwp /p:BuildProjectReferences=true
 msbuild %SOLUTION% /target:sqlite,zlib,sqlite-uwp /p:BuildProjectReferences=true /maxcpucount:%MAXCPUCOUNT% /p:Configuration=Release /p:Platform=x64
 
 REM Static /MT build
-msbuild %SOLUTION% /target:sqlite,zlib /p:BuildProjectReferences=true /maxcpucount:%MAXCPUCOUNT% /p:Configuration=Debug.vs2015.MT-sqlite /p:Platform=Win32
-msbuild %SOLUTION% /target:sqlite,zlib /p:BuildProjectReferences=true /maxcpucount:%MAXCPUCOUNT% /p:Configuration=Release.vs2015.MT-sqlite /p:Platform=Win32
-msbuild %SOLUTION% /target:sqlite,zlib /p:BuildProjectReferences=true /maxcpucount:%MAXCPUCOUNT% /p:Configuration=Debug.vs2015.MT-sqlite /p:Platform=x64
-msbuild %SOLUTION% /target:sqlite,zlib /p:BuildProjectReferences=true /maxcpucount:%MAXCPUCOUNT% /p:Configuration=Release.vs2015.MT-sqlite /p:Platform=x64
+msbuild %SOLUTION% /target:sqlite,zlib /p:BuildProjectReferences=true /maxcpucount:%MAXCPUCOUNT% /p:Configuration=Debug.vc14x.MT-sqlite /p:Platform=Win32
+msbuild %SOLUTION% /target:sqlite,zlib /p:BuildProjectReferences=true /maxcpucount:%MAXCPUCOUNT% /p:Configuration=Release.vc14x.MT-sqlite /p:Platform=Win32
+msbuild %SOLUTION% /target:sqlite,zlib /p:BuildProjectReferences=true /maxcpucount:%MAXCPUCOUNT% /p:Configuration=Debug.vc14x.MT-sqlite /p:Platform=x64
+msbuild %SOLUTION% /target:sqlite,zlib /p:BuildProjectReferences=true /maxcpucount:%MAXCPUCOUNT% /p:Configuration=Release.vc14x.MT-sqlite /p:Platform=x64
 
 REM ARM DLL build
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\vsdevcmd\ext\vcvars.bat" arm
