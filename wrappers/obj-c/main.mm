1faebd33839fe34a63a9f2991f9d0ed3ef22cf6b
Sid Dahiya <sidahiy@microsoft.com>
2021-07-14 10:28:35 -0700

    Add example in main.mm

--- wrappers/obj-c/main.mm
+++ wrappers/obj-c/main.mm
@@ -86,6 +86,7 @@ int main(int argc, char** argv){
 
         [logger2 logEventWithName:@"SemanticContext_EmptyEvent"];
         [ODWLogManager flushAndTeardown];
+        [ODWPrivacyGuard resetPrivacyGuardInstance];
     }
     return 0;
 }
