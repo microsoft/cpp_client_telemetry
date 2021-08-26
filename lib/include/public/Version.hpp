d2d2125727e4b83f354bc60197ed0fc515f98c83
Jeff Stong <jestong@microsoft.com>
2021-08-26 14:48:06 -0400

    revert unintended changes

--- lib/include/public/Version.hpp
+++ lib/include/public/Version.hpp
@@ -6,25 +6,28 @@
 #define MAT_VERSION_HPP
 // WARNING: DO NOT MODIFY THIS FILE!
 // This file has been automatically generated, manual changes will be lost.
-#define BUILD_VERSION_STR "3.5.237.1"
-#define BUILD_VERSION 3,5,237,1
+#define BUILD_VERSION_STR "3.5.200.1"
+#define BUILD_VERSION 3, 5, 200, 1
 
 #ifndef RESOURCE_COMPILER_INVOKED
 #include "ctmacros.hpp"
 #include <stdint.h>
 
-namespace MAT_NS_BEGIN {
+namespace MAT_NS_BEGIN
+{
+    uint64_t const Version =
+        ((uint64_t)3 << 48) |
+        ((uint64_t)5 << 32) |
+        ((uint64_t)200 << 16) |
+        ((uint64_t)1);
 
-uint64_t const Version =
-    ((uint64_t)3 << 48) |
-    ((uint64_t)5 << 32) |
-    ((uint64_t)237 << 16) |
-    ((uint64_t)1);
+}
+MAT_NS_END
 
-} MAT_NS_END
+namespace PAL_NS_BEGIN
+{
+}
+PAL_NS_END
 
-namespace PAL_NS_BEGIN { } PAL_NS_END
-
-#endif // RESOURCE_COMPILER_INVOKED
+#endif  // RESOURCE_COMPILER_INVOKED
 #endif
-
