c21b47e6937d554df76925570eabb8570c13ab7f
Sid Dahiya <53239974+sid-dahiya@users.noreply.github.com>
2022-02-22 15:05:33 -0800

    Fix breaks due to googletest migration

--- build-x64Debug.bat
+++ build-x64Debug.bat
@@ -3,5 +3,5 @@ cd %~dp0
 call tools\gen-version.cmd
 @setlocal ENABLEEXTENSIONS
 
-call tools\RunMsBuild.bat x64 Debug "sqlite:Rebuild,zlib:Rebuild,sqlite-uwp:Rebuild,win32-dll:Rebuild,win32-lib:Rebuild,net40:Rebuild,win10-cs:Rebuild,win10-dll:Rebuild,Tests\gmock:Rebuild,Tests\gtest:Rebuild,Tests\UnitTests:Rebuild,Tests\FuncTests:Rebuild"
+call tools\RunMsBuild.bat x64 Debug "sqlite:Rebuild,zlib:Rebuild,sqlite-uwp:Rebuild,win32-dll:Rebuild,win32-lib:Rebuild,net40:Rebuild,win10-cs:Rebuild,win10-dll:Rebuild,Tests\UnitTests:Rebuild,Tests\FuncTests:Rebuild"
 call tools\RunTests.bat x64 Debug
