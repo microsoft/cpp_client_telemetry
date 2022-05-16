38820bd9e62d7853aea0fb54b46d5833f6cd221b
t-ransemel <89340655+t-ransemel@users.noreply.github.com>
2022-05-16 12:22:16 +0300

    Suppress false positive CodeQL errors (#1013)
    * suppress false positive codeQL generated errors
    
    * add suppression justification
    
    * fixed typo

--- lib/include/mat/json.hpp
+++ lib/include/mat/json.hpp
@@ -6842,7 +6842,7 @@ class lexer : public lexer_base<BasicJsonType>
           locale's decimal point is used instead of `.` to work with the
           locale-dependent converters.
     */
-    token_type scan_number()  // lgtm [cpp/use-of-goto]
+    token_type scan_number()  // CodeQL [cpp/use-of-goto] We explicitly allow the use of goto in this func
     {
         // reset token_buffer to store the number's bytes
         reset();
