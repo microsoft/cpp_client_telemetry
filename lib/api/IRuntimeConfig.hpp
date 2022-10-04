74920afe00f60c10a9011a18442e4e5a7d80c54e
Matthew Kennedy <matthewkennedy@outlook.com>
2022-10-04 11:13:23 -0700

    Add config keys/getter for UTC changes (#1061)
    * Wire up provider init option
    
    * add config for "skip ikey reg"
    
    * unwind breaking change part for now
    
    * provider name getter
    
    * stray newline
    
    Co-authored-by: Matthew Kennedy <makenne@microsoft.com>

--- lib/api/IRuntimeConfig.hpp
+++ lib/api/IRuntimeConfig.hpp
@@ -192,6 +192,17 @@ namespace MAT_NS_BEGIN
         /// <returns>Provider Group Id</returns>
         virtual const char* GetProviderGroupId() = 0;
 
+        /// <summary>
+        /// Get UTC channel provider name
+        /// </summary>
+        /// <returns>Provider Name</returns>
+        virtual const char* GetProviderName() = 0;
+
+        /// <summary>
+        /// Get whether to skip registering the iKey with UTC
+        /// </summary>
+        virtual bool SkipIKeyRegistration() const = 0;
+
         virtual ~IRuntimeConfig() {};
     };
 
