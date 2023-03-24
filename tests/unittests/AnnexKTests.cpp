6bafd0baeceeeefa3106eddaadbbc7d88e00da8a
Lalit <labhas@microsoft.com>
2023-03-24 15:43:30 -0700

    fix unit tests

--- tests/unittests/AnnexKTests.cpp
+++ tests/unittests/AnnexKTests.cpp
@@ -20,8 +20,8 @@ using namespace MAT;
 
 TEST(AnnexKTests, memcpy_s)
 {
-    char    *dest = (char*) malloc(sizeof(char) * 10);
-    char    *src = (char*) malloc(sizeof(char) * 5);
+    void    *dest =  malloc(sizeof(char) * 10);
+    void    *src = malloc(sizeof(char) * 5);
     EXPECT_EQ(BoundCheckFunctions::oneds_memcpy_s(src, 5, "TEST", 5), 0);
     rsize_t dest_len = 10;
     rsize_t src_len = 4;
@@ -29,18 +29,18 @@ TEST(AnnexKTests, memcpy_s)
     // success tests
     EXPECT_EQ(BoundCheckFunctions::oneds_memcpy_s(dest, dest_len, src, 0), 0);
     EXPECT_EQ(BoundCheckFunctions::oneds_memcpy_s( dest, dest_len, src, src_len + 1), 0);
-    EXPECT_EQ(strlen(dest), strlen("TEST"));
+    EXPECT_EQ(strlen((char *)dest), strlen("TEST"));
     EXPECT_EQ(BoundCheckFunctions::oneds_memcpy_s(dest, dest_len + 2, src, src_len + 1), 0);
-    EXPECT_EQ(strlen(dest), strlen("TEST"));
+    EXPECT_EQ(strlen((char *)dest), strlen("TEST"));
 
     // error tests
     EXPECT_EQ(BoundCheckFunctions::oneds_memcpy_s(dest, 3, src, src_len), EINVAL);
-    EXPECT_EQ(dest[0], '\0');
+    EXPECT_EQ(((char *)dest)[0], '\0');
     EXPECT_EQ(BoundCheckFunctions::oneds_memcpy_s(NULL, 3, src, src_len), EINVAL);
     EXPECT_EQ(BoundCheckFunctions::oneds_memcpy_s( dest, dest_len, NULL, src_len), EINVAL);
-    EXPECT_EQ(dest[0], '\0');
+    EXPECT_EQ(((char *)dest)[0], '\0');
     EXPECT_EQ(BoundCheckFunctions::oneds_memcpy_s( dest, dest_len, src, dest_len + 1 ), EINVAL);
-    EXPECT_EQ(BoundCheckFunctions::oneds_memcpy_s( dest, dest_len, dest + 1, src_len + 1 ), EINVAL);
+    EXPECT_EQ(BoundCheckFunctions::oneds_memcpy_s( dest, dest_len, (void *)((char *)dest + 1), src_len + 1 ), EINVAL);
 }
 
 #if defined(__GNUC__) && !defined(__clang__) && !defined(__apple_build_version__)
