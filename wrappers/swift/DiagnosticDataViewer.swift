f8b3278dd83aff041b49d67a333ec41c3bdfb963
Abhinav Saroj <absaroj@microsoft.com>
2023-05-08 17:33:34 -0700

    adding class for swift diagnostic

--- wrappers/swift/DiagnosticDataViewer.swift
+++ wrappers/swift/DiagnosticDataViewer.swift
@@ -5,13 +5,81 @@
 
 /// Wrapper class over `ODWDiagnosticDataViewer` representing Diagnostic Data Viewer Hook.
 public class DiagnosticDataViewer {
+
+    /// MARK: Initialization Methods
+
     /**
     Initializes Data Viewer with a specified machine identifier.
 
     - Parameters:
         - machineIdentifier: `String` that contains the machine identifier.
     */
-    public static func initWithMachineIdentifier(_ machineIdentifier: String) {
+    public static func initWithMachineIdentifier(machineIdentifier: String) {
         ODWDiagnosticDataViewer.initializeWithMachineIdentifier(machineIdentifier)
+    }
+
+    /// MARK: Behavior Methods
+
+    /**
+    Enables Data Viewer.
+
+    - Parameters:
+        - endpoint: A `String` that contains endpoint to route events.
+        - completionWithResult: Code to execute when enable is completed.
+            - Note: This value can be null.
+    
+    Execution is dispatched to queue with default priority.
+    */
+    public static func enableRemoteViewer(endpoint: String, completionWithResult completion: (Bool) -> Void) {
+        ODWDiagnosticDataViewer.enableRemoteViewer(endpoint, completionWithResult: completion)
+    }
+
+    /**
+    Enables Data Viewer.
+
+    - Parameters:
+        - endpoint: A `String` that contains endpoint to route events.
+    */
+    public static func enableRemoteViewer(endpoint: String) -> Bool {
+        return ODWDiagnosticDataViewer.enableRemoteViewer(endpoint)
+    }
+
+    /**
+    Disables Data Viewer.
+
+    - Parameters:
+        - completionWithResult: Code to execute when disable is completed.
+            - Note: This value can be null.
+
+    Execution is dispatched to queue with default priority.
+    */
+    public static func disableViewer(completionWithResult completion: (Bool) -> Void) {
+        ODWDiagnosticDataViewer.disableViewer(completion)
+    }
+
+    /// Disables Data Viewer.
+    public static func disableViewer() -> Bool {
+        return ODWDiagnosticDataViewer. disableViewer()
+    }
+
+    /// Returns `True` if Data Viewer is enabled, `False` otherwise.
+    public static func viewerEnabled() -> Bool {
+        return ODWDiagnosticDataViewer.viewerEnabled()
+    }
+
+
+    /// Returns current endpoint if it is set, empty `String` otherwise.
+    public static func currentEndpoint() -> String {
+        return ODWDiagnosticDataViewer.currentEndpoint()
+    }
+
+    /**
+    Sets callback for OnDisableNotification event.
+
+    - Parameters:
+        - callback: Code to execute when OnDisableNotification event occurs.
+    */
+    public static func registerOnDisableNotification(callback: (Void) -> Void) {
+        ODWDiagnosticDataViewer.registerOnDisableNotification(callback)
     }
 }
