c24eb4ae5651986661cbab9868e0bdcd16527668
Abhinav Saroj <absaroj@microsoft.com>
2023-06-06 16:06:28 -0700

    changing the package name to 1dsswiftwrapper

--- wrappers/swift/Package.swift
+++ wrappers/swift/Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftWrapper",
+    name: "1DSSwiftWrapper",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
