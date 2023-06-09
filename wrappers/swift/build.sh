85d380aceab0d65bb42ef7eacfa9c6aa521f5a6b
noname <absaroj@microsoft.com>
2023-06-09 00:56:15 -0700

    adding swift wrappers build and linking

--- wrappers/swift/build.sh
+++ wrappers/swift/build.sh
@@ -5,7 +5,7 @@ export PATH=/usr/local/bin:$PATH
 # Build flavor
 BUILD_TYPE="debug"
 
-if [ "$1" != "clean" ]; then
+if [ "$1" == "release" ]; then
     BUILD_TYPE="$1"
 fi
 
@@ -14,10 +14,10 @@ if [ "$1" == "clean" ] || [ "$2" == "clean" ]; then
     echo "Cleaning output folders..."
     swift package clean
     rm -Rf out
-    rm -Rf ./build
+    rm -Rf .build
 fi
 
 # Generate the build files and run build
-mkdir out
+mkdir -p out/StaticLibs
 cd out
 cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..  -G Ninja # Generate swift project files for ninja
