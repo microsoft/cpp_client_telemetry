29278181abd68d2822cd3822b5ea55a1ba33e38a
noname <absaroj@microsoft.com>
2023-06-09 00:19:21 -0700

    creating static lib

--- wrappers/swift/build.sh
+++ wrappers/swift/build.sh
@@ -2,9 +2,14 @@
 
 export PATH=/usr/local/bin:$PATH
 
-BUILD_TYPE="${1:-debug}"
+# Build flavor
+BUILD_TYPE="debug"
 
-if [ "$2" == "clean" ]; then
+if [ "$1" != "clean" ]; then
+    BUILD_TYPE="$1"
+fi
+
+if [ "$1" == "clean" ] || [ "$2" == "clean" ]; then
     # Clean the output folders
     echo "Cleaning output folders..."
     swift package clean
@@ -15,4 +20,4 @@ fi
 # Generate the build files and run build
 mkdir out
 cd out
-cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..  -G Ninja # Generate swift project files for xcode
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..  -G Ninja # Generate swift project files for ninja
