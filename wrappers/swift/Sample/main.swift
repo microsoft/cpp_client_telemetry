7b697381d660722aefc3185d01444c66a1dd7548
noname <absaroj@microsoft.com>
2023-06-09 02:25:37 -0700

    adding comments

--- wrappers/swift/Sample/main.swift
+++ wrappers/swift/Sample/main.swift
@@ -3,8 +3,8 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-import ObjCModule
-import OneDSSwift
+import ObjCModule // Required if Enums defined in ObjC headers are being used
+import OneDSSwift // Swift Package containing the wrappers over ObjC
 
 
 let props = EventProperties(name:"TestEvent")
