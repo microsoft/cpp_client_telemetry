da20709703c38f47b990955c1e9852628e2f4886
noname <absaroj@microsoft.com>
2023-06-09 01:59:42 -0700

    adding more logic for building and producing sample app

--- wrappers/swift/Sample/main.swift
+++ wrappers/swift/Sample/main.swift
@@ -3,8 +3,11 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+import ObjCModule
+import OneDSSwift
+
+
 let props = EventProperties(name:"TestEvent")
 props.setProperty("PropName", withValue: ["Type":"SwiftWrappers"])
 props.setProperty("PropWithPII", withInt64Value: Int64(30), withPiiKind: ODWPiiKind.distinguishedName)
-print (props.properties)
-print (props.piiTags)
+print(props.properties())
