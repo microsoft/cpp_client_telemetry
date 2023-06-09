eb85e7fc5c8cbc3376970ed2e0296e2326faba54
noname <absaroj@microsoft.com>
2023-06-08 23:45:19 -0700

    adding build.sh changes

--- wrappers/swift/build.sh
+++ wrappers/swift/build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 export PATH=/usr/local/bin:$PATH
+
+BUILD_TYPE="${1:-debug}"
+
+if [ "$2" == "clean" ]; then
+    # Clean the output folders
+    echo "Cleaning output folders..."
+    swift package clean
+    rm -Rf out
+    rm -Rf ./build
+fi
+
+# Generate the build files and run build
 mkdir out
 cd out
-cmake .. -G Xcode # Generate swift project files for xcode
-xcodebuild -quiet
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..  -G Ninja # Generate swift project files for xcode
