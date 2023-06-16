c2ddcb98092f740b23cef975b5c47601399f1a42
noname <absaroj@microsoft.com>
2023-06-16 01:29:33 -0700

    adding call to create logger

--- wrappers/swift/Sample/main.swift
+++ wrappers/swift/Sample/main.swift
@@ -9,3 +9,6 @@ let props = EventProperties(name:"TestEvent")
 props.setProperty("PropName", withValue: ["Type":"SwiftWrappers"])
 props.setProperty("PropWithPII", withInt64Value: Int64(30), withPiiKind: PIIKind.distinguishedName)
 print(props.properties())
+
+let token: String = "7c8b1796cbc44bd5a03803c01c2b9d61-b6e370dd-28d9-4a52-9556-762543cf7aa7-6991"
+let myLogger: Logger = LogManager.loggerWithTenant(tenantToken: token)!
