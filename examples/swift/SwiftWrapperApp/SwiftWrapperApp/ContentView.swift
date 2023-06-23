41492f936ebda9d256ce2e792b48bcba4690ae82
Abhinav Saroj <absaroj@microsoft.com>
2023-06-23 16:36:04 -0700

    fixing a bug and correcting sample app projectclear

--- examples/swift/SwiftWrapperApp/SwiftWrapperApp/ContentView.swift
+++ examples/swift/SwiftWrapperApp/SwiftWrapperApp/ContentView.swift
@@ -8,9 +8,12 @@
 import OneDSSwift
 import SwiftUI
 
+private var logger:Logger?
+private var token:String = ""
+
 struct ContentView: View {
 	@State private var enteredText = ""
-	
+
     var body: some View {
 		VStack {
 			TextField("Enter text", text: $enteredText)
@@ -31,21 +34,12 @@ struct ContentView: View {
 			}
 		}
 		.padding()
-	}
-	
-	/// Method accessing Swift Wrapper package OneDSSwift telemetry classes
-	func sendTelemetry(_ text:String) {
-		let event:EventProperties = EventProperties(name: "SetProps_Swift_Wrapper_Example_Proj")
-		event.setProperty("enteredtext", withValue: text)
-		event.setProperty("from", withValue: "Swift_Wrappers_Package_Example_Project")
-		
-		let logger: Logger? = LogManager.loggerForSource(source: "example_proj_source")
-		if (logger != nil) {
-			logger!.logEvent(properties: event)
+		.onAppear() {
+			initTelemetry()
 		}
-		
-		LogManager.uploadNow()
-		_ = LogManager.flushAndTeardown()
+		.onDisappear() {
+			closeTelemetrySession()
+		}
 	}
 }
 
@@ -53,4 +47,26 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+/// Method accessing Swift Wrapper package OneDSSwift telemetry classes
+
+func initTelemetry() {
+	token = "7c8b1796cbc44bd5a03803c01c2b9d61-b6e370dd-28d9-4a52-9556-762543cf7aa7-6991"
+	logger = LogManager.loggerWithTenant(tenantToken: token, withSource: "example_proj_source")
+}
+
+func sendTelemetry(_ text:String) {
+	let event:EventProperties = EventProperties(name: "SetProps_Swift_Wrapper_Example_Proj")
+	event.setProperty("enteredtext", withValue: text)
+	event.setProperty("from", withValue: "Swift_Wrappers_Package_Example_Project")
+
+	if (logger != nil) {
+		logger!.logEvent(properties: event)
+	}
+	LogManager.uploadNow()
+}
+
+func closeTelemetrySession() {
+	_ = LogManager.flushAndTeardown()
 }
