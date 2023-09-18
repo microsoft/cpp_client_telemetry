1af6f6ec552fa7f030c2a4dc7c7bcedc258be5d4
Shiv Pratap Singh <ssingh@microsoft.com>
2023-09-17 23:00:54 -0700

    Update 1 upload

--- lib/tpm/TransmissionPolicyManager.cpp
+++ lib/tpm/TransmissionPolicyManager.cpp
@@ -357,7 +357,7 @@ namespace MAT_NS_BEGIN {
 
         // Introducing a 40-second delay before forcefully scheduling the upload job, to ensure it happens at an optimal time.
         // This delay is implemented to address Issue 388, where the last cancellation might have been halted due to the issue described below.
-        if (other_priority_elapsed_seconds > 5 && m_isUploadScheduled.exchange(false)){
+        if (other_priority_elapsed_seconds > 1){
             LOG_TRACE("Trigger upload on event arrival");
             otherPriorityLastExecutionTime = currentTime;
         }
