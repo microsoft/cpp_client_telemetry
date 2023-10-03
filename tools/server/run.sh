c299779fcf754de003b3d7ebbb7e4669c3a35fff
Inna Ankudinova <iankudinova@microsoft.com>
2023-10-03 17:52:09 +0100

    Update scripts, that runs test server to specify flags for target frameworks

--- tools/server/run.sh
+++ tools/server/run.sh
@@ -1,3 +1,39 @@
 #!/bin/sh
-dotnet run
+
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
+   echo "Run 1DS Test Telemetry Server with specified runtime versions (.net core 3 or .net6)."
+   echo
+   echo "Syntax: run.sh [-f,--framework|-h, --help]"
+   echo "options:"
+   echo "-f | --framework     Run server with specified version of runtime. Supported values: netcoreapp3.1 or net6.0. Default value is netcoreapp3.1, if option is not specified"
+   echo "-h | --help    Help."
+   echo
+
+   exit 0
+}
+
+framework="netcoreapp3.1"
+while test $# -gt 0;
+do
+  case "$1" in
+    -h|--help) Help;;
+    -f|--framework)       
+      shift
+      if test $# -gt 0; then
+        framework=$1
+      else
+        echo "framework is not specified. Server will be run with .netcoreapp3.1"
+      fi
+      shift
+      ;;
+    *)  break;;
+  esac
+done
+echo "1DS test telemetry server will be run with $framework"
+dotnet run -f $framework
 
