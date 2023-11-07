0847871e9a1cce38e07dd98318faf847c7d983a9
Shiv Pratap Singh <ssingh@microsoft.com>
2023-11-06 22:06:59 -0800

    Upgrade zlib

--- zlib/inftrees.c
+++ zlib/inftrees.c
@@ -9,7 +9,7 @@
 #define MAXBITS 15
 
 const char inflate_copyright[] =
-   " inflate 1.2.11 Copyright 1995-2017 Mark Adler ";
+   " inflate 1.2.12 Copyright 1995-2017 Mark Adler ";
 /*
   If you use the zlib library in a product, an acknowledgment is welcome
   in the documentation of your product. If for some reason you cannot
