d60ff6da4908ab2b2c99dfd0aa2edb2ef43eefb6
Tomasz Kukielka <tkukielk@microsoft.com>
2024-05-25 00:00:38 -0700

    Fix build scripts and add visionOS support
    Multiple fixes:
    - building on Apple Silicon Macs - remove the assumptions that the build machine is always x86_64
    - fix badly broken build.sh for Mac, especially with getopts for options & params
    - extend build-ios.sh to allow building for iOS-derived platforms like visionOS
    
    The expected iOS build invocation is:
    build-ios.sh [clean] [release|debug] ${ARCH} ${PLATFORM}
    where
    ARCH = arm64|arm64e|x86_64
    PLATFORM = iphoneos|iphonesimulator|xros|xrsimulator

--- build.sh
+++ build.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+usage()
+{
+  echo "Usage: build.sh [clean] [arm64|x86_64|universal] [CUSTOM_CMAKE_CXX_FLAGS=x] [noroot] [release] [-h|-?] [-l (static|shared)] [-D CMAKE_OPTION] [-v]"
+  echo "                                                                                         "
+  echo "options:                                                                                 "
+  echo "                                                                                         "
+  echo "Positional options (1st three arguments):                                                "
+  echo "[clean]                  - perform clean build                                           "
+  echo "[arm64|x86_64|universal] - Apple platform build type. Not applicable to other OS.        "
+  echo "[CUSTOM_CMAKE_CXX_FLAGS] - custom CXX compiler flags                                     "
+  echo "[noroot]                 - custom CXX compiler flags                                     "
+  echo "[release]                - build for Release                                             "
+  echo "                                                                                         "
+  echo "Additional parameters:                                                                   "
+  echo " -h | -?                 - this help.                                                    "
+  echo " -l [static|shared]      - build static (default) or shared library.                     "
+  echo " -D [CMAKE_OPTION]       - additional options to pass to cmake. Could be multiple.       "
+  echo " -v                      - increase build verbosity (reserved for future use)            "
+  echo "                                                                                         "
+  echo "Environment variables:                                                                   "
+  echo "CMAKE_OPTS               - any additional cmake options.                                 "
+  echo "GIT_PULL_TOKEN           - authorization token for Microsoft-proprietary modules.        "
+  echo "MACOSX_DEPLOYMENT_TARGET - optional parameter for setting macosx deployment target       "
+  echo "Plus any other environment variables respected by CMake build system.                    "
+  exit 0
+}
+
 export PATH=/usr/local/bin:$PATH
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -8,78 +35,80 @@ cd $DIR
 
 export NOROOT=$NOROOT
 
-if [ "$1" == "clean" ]; then
+PARAM1="$1"
+PARAM2="$2"
+PARAM3="$3"
+
+if [ "$PARAM1" == "clean" ]; then
+  echo "Cleaning previous build artifacts"
   rm -f CMakeCache.txt *.cmake
   rm -rf out
   rm -rf .buildtools
   # make clean
+  shift
 fi
 
-if [ "$1" == "noroot" ] || [ "$2" == "noroot" ] || [ "$3" == "noroot" ]; then
+if [ "$PARAM1" == "noroot" ] || [ "$PARAM2" == "noroot" ] || [ "$PARAM3" == "noroot" ]; then
   export NOROOT=true
+  echo "NOROOT = true"
+  shift
 fi
 
-if [ "$1" == "release" ] || [ "$2" == "release" ] || [ "$3" == "release" ]; then
+if [ "$PARAM1" == "release" ] || [ "$PARAM2" == "release" ] || [ "$PARAM3" == "release" ]; then
   BUILD_TYPE="Release"
+  echo "BUILD_TYPE = Release"
+  shift
+elif [ "$PARAM1" == "debug" ] || [ "$PARAM2" == "debug" ] || [ "$PARAM3" == "debug" ]; then
+  BUILD_TYPE="Debug"
+  echo "BUILD_TYPE = Debug"
+  shift
 else
   BUILD_TYPE="Debug"
+  echo "Assuming default BUILD_TYPE = Debug"
 fi
 
-if [ "$1" == "arm64" ] || [ "$2" == "arm64" ] || [ "$3" == "arm64" ]; then
+if [ "$PARAM1" == "arm64" ] || [ "$PARAM2" == "arm64" ] || [ "$PARAM3" == "arm64" ]; then
   MAC_ARCH="arm64"
-elif [ "$1" == "universal" ] || [ "$2" == "universal" ] || [ "$3" == "universal" ]; then
+  echo "MAC_ARCH = arm64"
+  shift
+elif [ "$PARAM1" == "universal" ] || [ "$PARAM2" == "universal" ] || [ "$PARAM3" == "universal" ]; then
   MAC_ARCH="universal"
+  echo "MAC_ARCH = universal"
+  shift
+elif [ "$PARAM1" == "x86_64" ] || [ "$PARAM2" == "x86_64" ] || [ "$PARAM3" == "x86_64" ]; then
+  MAC_ARCH="x86_64"
+  echo "MAC_ARCH = x86_64"
+  shift
 else
   MAC_ARCH="x86_64"
+  echo "Assuming default MAC_ARCH = x86_64"
 fi
 
 CUSTOM_CMAKE_CXX_FLAG=""
-if [[ $1 == CUSTOM_BUILD_FLAGS* ]] || [[ $2 == CUSTOM_BUILD_FLAGS* ]] || [[ $3 == CUSTOM_BUILD_FLAGS* ]]; then
-  if [[ $1 == CUSTOM_BUILD_FLAGS* ]]; then
+if [[ $PARAM1 == CUSTOM_BUILD_FLAGS* ]] || [[ $PARAM2 == CUSTOM_BUILD_FLAGS* ]] || [[ $PARAM3 == CUSTOM_BUILD_FLAGS* ]]; then
+  if [[ $PARAM1 == CUSTOM_BUILD_FLAGS* ]]; then
   CUSTOM_CMAKE_CXX_FLAG="\"${1:19:999}\""
-  elif [[ $2 == CUSTOM_BUILD_FLAGS* ]]; then
+  elif [[ $PARAM2 == CUSTOM_BUILD_FLAGS* ]]; then
   CUSTOM_CMAKE_CXX_FLAG="\"${2:19:999}\""
-  elif [[ $3 == CUSTOM_BUILD_FLAGS* ]]; then
+  elif [[ $PARAM3 == CUSTOM_BUILD_FLAGS* ]]; then
   CUSTOM_CMAKE_CXX_FLAG="\"${3:19:999}\""
   fi
-  echo "custom build flags="$CUSTOM_CMAKE_CXX_FLAG
+  shift
+  echo "custom build flags = $CUSTOM_CMAKE_CXX_FLAG"
 fi
 
 LINK_TYPE=
 CMAKE_OPTS="${CMAKE_OPTS:--DBUILD_SHARED_LIBS=OFF}"
 while getopts "h?vl:D:" opt; do
     case "$opt" in
-    h|\?)
-        echo "Usage: build.sh [clean] [arm64|universal] [CUSTOM_CMAKE_CXX_FLAGS=x] [noroot] [release] [-h|-?] [-l (static|shared)] [-D CMAKE_OPTION] [-v]"
-        echo "                                                                                         "
-        echo "options:                                                                                 "
-        echo "                                                                                         "
-        echo "Positional options (1st three arguments):                                                "
-        echo "[clean]                  - perform clean build                                           "
-        echo "[arm64|universal]        - Apple platform build type. Not applicable to other OS.        "
-        echo "[CUSTOM_CMAKE_CXX_FLAGS] - custom CXX compiler flags                                     "
-        echo "[noroot]                 - custom CXX compiler flags                                     "
-        echo "[release]                - build for Release                                             "
-        echo "                                                                                         "
-        echo "Additional parameters:                                                                   "
-        echo " -h | -?                 - this help.                                                    "
-        echo " -l [static|shared]      - build static (default) or shared library.                     "
-        echo " -D [CMAKE_OPTION]       - additional options to pass to cmake. Could be multiple.       "
-        echo " -v                      - increase build verbosity (reserved for future use)            "
-        echo "                                                                                         "
-        echo "Environment variables:                                                                   "
-        echo "CMAKE_OPTS               - any additional cmake options.                                 "
-        echo "GIT_PULL_TOKEN           - authorization token for Microsoft-proprietary modules.        "
-        echo "MACOSX_DEPLOYMENT_TARGET - optional parameter for setting macosx deployment target       "
-        echo "Plus any other environment variables respected by CMake build system.                    "
-        exit 0
+    h|\?) usage
         ;;
     :)  echo "Invalid option: $OPTARG requires an argument" 1>&2
         exit 0
         ;;
     v)  verbose=1
         ;;
-    D)  CMAKE_OPTS="$CMAKE_OPTS -D$OPTARG"
+    D)  CMAKE_OPTS="${CMAKE_OPTS} -D${OPTARG}"
         ;;
     l)  LINK_TYPE=$OPTARG
         ;;
@@ -87,8 +116,10 @@ while getopts "h?vl:D:" opt; do
 done
 shift $((OPTIND -1))
 
+echo "CMAKE_OPTS from caller: $CMAKE_OPTS"
+
 if [ "$LINK_TYPE" == "shared" ]; then
-  CMAKE_OPTS="$CMAKE_OPTS -DBUILD_SHARED_LIBS=ON"
+  CMAKE_OPTS="${CMAKE_OPTS} -DBUILD_SHARED_LIBS=ON"
 fi
 
 # Set target MacOS minver
