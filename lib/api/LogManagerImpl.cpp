3d8432545eae5309788b92f697397e0df7bcf12d
fmarasco <fmarasco+odspmdb@microsoft.com>
2025-03-14 08:16:31 -0400

    add module

--- lib/api/LogManagerImpl.cpp
+++ lib/api/LogManagerImpl.cpp
@@ -85,6 +85,19 @@
 #endif
 #endif
 
+#ifdef HAVE_MAT_SANITIZER
+#if defined __has_include
+#if __has_include("modules/sanitizer/Sanitizer.hpp")
+#include "modules/sanitizer/Sanitizer.hpp"
+#else
+/* Compiling without Sanitizer support because Santizer private header is unavailable */
+#undef HAVE_MAT_SANITIZER
+#endif
+#else
+#include "modules/sanitizer/Sanitizer.hpp"
+#endif
+#endif
+
 namespace MAT_NS_BEGIN
 {
     void DeadLoggers::AddMap(LoggerMap&& source)
