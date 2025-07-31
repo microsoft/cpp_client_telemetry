0f4008eb88e2c2f3f2b9c7fbcedfa87025a26ae2
fmarasco <fmarasco+odspmdb@microsoft.com>
2025-07-31 17:55:21 -0400

    updating version to include module wrapper

--- lib/include/public/Version.hpp
+++ lib/include/public/Version.hpp
@@ -6,8 +6,8 @@
 #define MAT_VERSION_HPP
 // WARNING: DO NOT MODIFY THIS FILE!
 // This file has been automatically generated, manual changes will be lost.
-#define BUILD_VERSION_STR "3.9.205.1"
-#define BUILD_VERSION 3,9,205,1
+#define BUILD_VERSION_STR "3.9.212.1"
+#define BUILD_VERSION 3,9,212,1
 
 #ifndef RESOURCE_COMPILER_INVOKED
 #include "ctmacros.hpp"
@@ -18,7 +18,7 @@ namespace MAT_NS_BEGIN {
 uint64_t const Version =
     ((uint64_t)3 << 48) |
     ((uint64_t)9 << 32) |
-    ((uint64_t)205 << 16) |
+    ((uint64_t)212 << 16) |
     ((uint64_t)1);
 
 } MAT_NS_END
