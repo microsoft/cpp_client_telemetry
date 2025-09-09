effdb935ed39ae643aa0b6f653a3a8330f58b4c4
fmarasco <fmarasco+odspmdb@microsoft.com>
2025-09-09 07:55:23 -0400

    udpate ios code

--- wrappers/obj-c/ODWSanitizer.mm
+++ wrappers/obj-c/ODWSanitizer.mm
@@ -31,7 +31,7 @@ std::shared_ptr<Sanitizer> _sanitizerPtr;
     {
         config.NotificationEventName = [[initConfigObject notificationEventName] UTF8String];
     }
-    config.SetAllWarningsToSanitization = [initConfigObject setAllWarningsToSanitization];
+    config.SetAllWarningsToSanitizations = [initConfigObject setAllWarningsToSanitization];
 
     _sanitizerPtr = std::make_shared<Sanitizer>(config);
     LogManager::GetInstance()->SetDataInspector(_sanitizerPtr);
