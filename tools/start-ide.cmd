f12b17005d210ed576ee437ee58f56c6a4b0675e
Alvaro Palma Aste <apalmaaste@microsoft.com>
2026-01-02 13:41:49 -0800

    Update to SQLite 3.50.4 Update ZLib 1.3.1. Update NLohmann's Json to 3.12.0 Update compilation for Windows to use VS 2022 Fix compilation for MacOS and iOS ARM platforms

--- tools/start-ide.cmd
+++ tools/start-ide.cmd
@@ -3,8 +3,8 @@ set "ROOT=%~dp0\.."
 REM Specify custom build options: uncomment line below pointing to custom build configuration
 REM set "CUSTOM_PROPS_VS=%ROOT%\Solutions\build.compact-min.props"
 
-REM This script assumes that local IDE is Visual Studio 2019 Enterprise + toolchains (vc141, vc142, etc.)
-REM Edit line below if your IDE is different than Visual Studio 2019
-set "IDE=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\devenv.exe"
+REM This script assumes that local IDE is Visual Studio 2022 Enterprise + toolchains (vc143, etc.)
+REM Edit line below if your IDE is different than Visual Studio 2022
+set "IDE=%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.exe"
 
 "%IDE%" "%ROOT%\Solutions\MSTelemetrySDK.sln"
